1bc23de56d2d004c50b440df94ed37eb353aa75d
koesnam <76582376+10kseok@users.noreply.github.com>
2023-06-30 21:12:04 +0900

    configuration

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/CookshoongBackendApplication.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshoongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookshoongBackendApplication.class, args);
+    }
+
+}
