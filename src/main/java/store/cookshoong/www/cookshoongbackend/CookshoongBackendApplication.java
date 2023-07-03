d8ac4c27427bcc4ca14cae37625514216e2bc87d
윤동현 <eatpaper1429@gmail.com>
2023-07-03 12:25:50 +0900

    refactor: 💡 추가: slf4j annotation 추가
    test slf4j annotation

--- src/main/java/store/cookshoong/www/cookshoongbackend/CookshoongBackendApplication.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/CookshoongBackendApplication.java
@@ -1,13 +1,15 @@
 package store.cookshoong.www.cookshoongbackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class CookshoongBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CookshoongBackendApplication.class, args);
+		log.info("System Activated");
     }
 
 }
