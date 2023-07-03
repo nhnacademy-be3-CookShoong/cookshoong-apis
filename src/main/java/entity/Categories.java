61fb4c2548fb2a382cddb003f200377b1e9122aa
eora21 <eora21@naver.com>
2023-07-03 10:17:33 +0900

    refactor: 💡 변경 @Data 제거

--- src/main/java/entity/Categories.java
+++ src/main/java/entity/Categories.java
@@ -1,25 +1,19 @@
 package entity;
 
-import lombok.Data;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Serializable;
 
-@Data
 @Entity
 @Table(name = "categories")
-public class Categories implements Serializable {
+public class Categories {
 
-    private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "category_code", nullable = false)
+	private String categoryCode;
 
-    @Id
-    @Column(name = "category_code", nullable = false)
-    private String categoryCode;
-
-    @Column(name = "description", nullable = false)
-    private String description;
+	@Column(name = "description", nullable = false)
+	private String description;
 
 }
