2c67a900391f9b00ec8656df26c41fbb6df63874
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-04 22:40:49 +0900

    feat: 🎸 추가 회원 관련 레포지토리 작성
    구현된 엔터티(회원, 회원 상태, 권한, 소셜 로그인, 소셜 로그인 타입, 회원등급) 모두 레포지토리 생성하였습니다.

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/account/repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package store.cookshoong.www.cookshoongbackend.account.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.cookshoong.www.cookshoongbackend.account.entity.Account;
+
+/**
+ * 회원 CRU 를 위한 레포지토리.
+ *
+ * @author koesnam
+ * @since 2023.07.04
+ */
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByLoginId(String loginId);
+}
