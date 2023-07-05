5cbe30067327494bd609102b22c026eedefc2c1e
정제완 <jewan8821@naver.com>
2023-07-05 20:46:29 +0900

    refactor: 💡 수정 GetMapping 을 PostMapping 으로 변경
    AddressControler createAccountAddress Mapping 수정

--- src/main/java/store/cookshoong/www/cookshoongbackend/address/controller/AddressController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/address/controller/AddressController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class AddressController {
      * @param requestDto    회원이 등록하고자 하는 주소
      * @return              상태코드 201(CREATED)와 함께 응답을 반환
      */
-    @GetMapping("/{accountId}")
+    @PostMapping("/{accountId}")
     public ResponseEntity<Void> createAccountAddress(@PathVariable("accountId") Long accountId,
                                                      @RequestBody CreateAccountAddressRequestDto requestDto) {
 
