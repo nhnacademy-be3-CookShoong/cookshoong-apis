4078845a8f6541c39520442a670654389ed3c6b2
eora21 <eora21@naver.com>
2023-07-05 13:58:47 +0900

    feat: 🎸 추가 가게에서 포인트 쿠폰 정책을 생성할 때 사용되는 dto 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/model/request/CreateStorePercentCouponPolicyRequestDto.java
@@ -0,0 +1,58 @@
+package store.cookshoong.www.cookshoongbackend.coupon.model.request;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypePercent;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsage;
+
+/**
+ * 가게에서 포인트 쿠폰 정책을 생성할 때 사용되는 dto.
+ *
+ * @author eora21
+ * @since 2023.07.04
+ */
+@Getter
+@AllArgsConstructor
+public class CreateStorePercentCouponPolicyRequestDto {
+    private String name;
+
+    private String description;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalTime expirationTime;
+
+    private BigDecimal rate;
+
+    private int minimumPrice;
+
+    private int maximumPrice;
+
+    /**
+     * dto 값을 이용하여 CouponTypeCash 엔티티를 생성하는 static method.
+     *
+     * @param dto the dto
+     * @return the coupon type percent
+     */
+    public static CouponTypePercent toCouponTypePercent(CreateStorePercentCouponPolicyRequestDto dto) {
+        return new CouponTypePercent(dto.getRate(), dto.getMinimumPrice(), dto.getMaximumPrice());
+    }
+
+    /**
+     * dto, 쿠폰 타입, 쿠폰 사용처를 이용하여 CouponPolicy 엔티티를 생성하는 static method.
+     *
+     * @param couponTypePercent the coupon type percent
+     * @param couponUsage       the coupon usage
+     * @param dto               the dto
+     * @return the coupon policy
+     */
+    public static CouponPolicy toCouponPolicy(CouponTypePercent couponTypePercent, CouponUsage couponUsage,
+                                              CreateStorePercentCouponPolicyRequestDto dto) {
+        return new CouponPolicy(couponTypePercent, couponUsage, dto.getName(), dto.getDescription(),
+            dto.getExpirationTime());
+    }
+}
+
