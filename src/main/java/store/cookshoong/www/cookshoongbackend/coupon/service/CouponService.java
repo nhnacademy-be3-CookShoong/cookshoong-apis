95a06f056320d0e25a3069c244c45e817a6e96d7
eora21 <eora21@naver.com>
2023-07-05 17:37:06 +0900

    feat: 🎸 추가 가맹점 금액, 포인트 쿠폰 정책 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/CouponService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/CouponService.java
@@ -8,6 +8,7 @@ import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponType;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypeCash;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypePercent;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsage;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageMerchant;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageStore;
 import store.cookshoong.www.cookshoongbackend.coupon.model.request.CouponPolicyRequest;
 import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreateCashCouponPolicyRequestDto;
@@ -15,6 +16,7 @@ import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreatePercent
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponPolicyRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponTypeCashRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponTypePercentRepository;
+import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponUsageMerchantRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponUsageStoreRepository;
 
 /**
@@ -30,6 +32,7 @@ public class CouponService {
     private final CouponTypeCashRepository couponTypeCashRepository;
     private final CouponTypePercentRepository couponTypePercentRepository;
     private final CouponUsageStoreRepository couponUsageStoreRepository;
+    private final CouponUsageMerchantRepository couponUsageMerchantRepository;
     private final CouponPolicyRepository couponPolicyRepository;
 
     /**
@@ -62,6 +65,36 @@ public class CouponService {
             .getId();
     }
 
+    /**
+     * 가맹점 금액 쿠폰 정책 생성.
+     *
+     * @param merchantId the merchant id
+     * @param dto        he creation store cash coupon request dto
+     * @return the long
+     */
+    public Long createMerchantCashCouponPolicy(Long merchantId, CreateCashCouponPolicyRequestDto dto) {
+        CouponTypeCash couponTypeCash = getOrCreateCouponTypeCash(dto);
+        CouponUsageMerchant couponUsageMerchant = getOrCreateCouponUsageMerchant(merchantId);
+
+        return createCouponPolicy(couponTypeCash, couponUsageMerchant, dto)
+            .getId();
+    }
+
+    /**
+     * 가맹점 포인트 쿠폰 정책 생성.
+     *
+     * @param merchantId the merchant id
+     * @param dto        he creation store point coupon request dto
+     * @return the long
+     */
+    public Long createMerchantPercentCouponPolicy(Long merchantId, CreatePercentCouponPolicyRequestDto dto) {
+        CouponTypePercent couponTypePercent = getOrCreateCouponTypePercent(dto);
+        CouponUsageMerchant couponUsageMerchant = getOrCreateCouponUsageMerchant(merchantId);
+
+        return createCouponPolicy(couponTypePercent, couponUsageMerchant, dto)
+            .getId();
+    }
+
     private CouponTypeCash getOrCreateCouponTypeCash(CreateCashCouponPolicyRequestDto dto) {
         return couponTypeCashRepository.findByDiscountAmountAndMinimumPrice(
                 dto.getDiscountAmount(), dto.getMinimumPrice())
@@ -79,6 +112,11 @@ public class CouponService {
     private CouponUsageStore getOrCreateCouponUsageStore(Long storeId) {
         return couponUsageStoreRepository.findByStoreId(storeId)
             .orElseGet(() -> couponUsageStoreRepository.save(new CouponUsageStore(storeId)));
+    }
+
+    private CouponUsageMerchant getOrCreateCouponUsageMerchant(Long merchantId) {
+        return couponUsageMerchantRepository.findByMerchantId(merchantId)
+            .orElseGet(() -> couponUsageMerchantRepository.save(new CouponUsageMerchant(merchantId)));
     }
 
     private CouponPolicy createCouponPolicy(CouponType couponType, CouponUsage couponUsage, CouponPolicyRequest req) {
