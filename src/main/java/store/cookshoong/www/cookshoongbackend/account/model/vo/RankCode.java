81d9deb3e3000b656ff3bbe5d594fa9d609b796f
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-05 16:41:14 +0900

    refactor: 💡 추가  회원 상태코드, 등급코드 작성
    각 상태코드들은 미리 DB에 있을 데이터이므로 Enum으로 만들어 관리하게 한다.

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/account/model/vo/RankCode.java
@@ -0,0 +1,24 @@
+package store.cookshoong.www.cookshoongbackend.account.model.vo;
+
+/**
+ * 회원 등급코드들을 상수로 관리하기 위한 Enum
+ *
+ * @author koesnam
+ * @since 2023.07.05
+ */
+public enum RankCode {
+    LEVEL_1("프랜드"),
+    LEVEL_2("패밀리"),
+    LEVEL_3("마스터"),
+    LEVEL_4("VIP");
+
+    private final String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    RankCode(String description) {
+        this.description = description;
+    }
+}
