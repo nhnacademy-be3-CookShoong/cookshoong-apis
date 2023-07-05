920e1ee75ae60ab220557e4e3a863aae26c8c4cc
eora21 <eora21@naver.com>
2023-07-05 10:57:54 +0900

    feat: 🎸 추가 쿠폰 RestController 및 매장 금액 쿠폰 정책 생성을 위한 엔드포인트 작성
    javdoc은 구현해야 할 로직들에 대해 엔드포인트 부분에서 고려하기 위해 작성. 자세한 설명들은 추후 서비스로 옮길 것.

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/controller/CouponController.java
@@ -0,0 +1,44 @@
+package store.cookshoong.www.cookshoongbackend.coupon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreateStoreCashCouponPolicyRequestDto;
+import store.cookshoong.www.cookshoongbackend.service.CouponService;
+
+/**
+ * 쿠폰 RestController.
+ * 쿠폰 타입, 사용처 지정과 전체 정책, 발행, 내역 확인을 담당한다.
+ * 소유 쿠폰 조회가 가능하도록 작성하며, 페이징과 필터링 모두 가능하도록 한다.
+ *
+ * @author eora21
+ * @since 2023.07.04
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/coupon")
+public class CouponController {
+    private final CouponService couponService;
+
+
+    /**
+     * 매장 금액 쿠폰 정책 생성을 위한 엔드포인트.
+     *
+     * @param storeId the store id
+     * @param dto     가게에서 쿠폰 금액 정책을 생성할 때 사용되는 dto
+     * @return CREATED status 및 쿠폰 정책 id
+     */
+    @PostMapping("store/{storeId}/cash")
+    public ResponseEntity<Long> createStoreCashCouponPolicy(@PathVariable Long storeId,
+                                                            CreateStoreCashCouponPolicyRequestDto dto) {
+        Long storeCashCouponPolicyId = couponService.createStoreCashCouponPolicy(storeId, dto);
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(storeCashCouponPolicyId);
+    }
+}
