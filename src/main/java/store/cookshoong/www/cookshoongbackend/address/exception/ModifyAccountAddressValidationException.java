2350c4e1f4c1280b0e22d4d43c84a89045f7fb1b
정제완 <jewan8821@naver.com>
2023-07-06 22:59:00 +0900

    refactor: 💡 수정 ValidationException 에 대한 javadoc 수정

--- src/main/java/store/cookshoong/www/cookshoongbackend/address/exception/ModifyAccountAddressValidationException.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/address/exception/ModifyAccountAddressValidationException.java
@@ -4,9 +4,9 @@ import org.springframework.validation.BindingResult;
 import store.cookshoong.www.cookshoongbackend.common.exception.ValidationFailureException;
 
 /**
- * 회원가입 요청 데이터의 위반사항 발견 발생되는 예외.
+ * 회원의 주소에 대한 Valid Exception.
  *
- * @author koesnam
+ * @author jeongjewan
  * @since 2023.07.05
  */
 public class ModifyAccountAddressValidationException extends ValidationFailureException {
