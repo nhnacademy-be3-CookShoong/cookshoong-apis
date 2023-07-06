2edf684fc8105d3e98d1205485c5ee4190437dd4
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-06 14:15:44 +0900

    feat: :guitar: 추가 가맹점 CRUD

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/store/model/response/MerchantResponseDto.java
@@ -0,0 +1,11 @@
+package store.cookshoong.www.cookshoongbackend.store.model.response;
+
+/**
+ * 가맹점 등록 및 수정을 위한 Dto.
+ *
+ * @author seungyeon
+ * @since 2023.07.06
+ */
+public class MerchantResponseDto {
+    private String merchantName;
+}
