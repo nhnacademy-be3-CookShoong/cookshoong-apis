2f22e4ede240c368a0d419985f117ae2a8023fab
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-07 17:23:09 +0900

    feat: :guitar: 추가 Merchant 테스트 코드 작성

---
+++ src/test/java/store/cookshoong/www/cookshoongbackend/store/repository/MerchantRepositoryTest.java
@@ -0,0 +1,79 @@
+package store.cookshoong.www.cookshoongbackend.store.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import store.cookshoong.www.cookshoongbackend.store.entity.Merchant;
+
+/**
+ * 가맹점 레포지토리 테스트코드 작성.
+ *
+ * @author seungyeon
+ * @since 2023.07.07
+ */
+@DataJpaTest
+class MerchantRepositoryTest {
+    @MockBean
+    JPAQueryFactory jpaQueryFactory;
+
+    @Autowired
+    TestEntityManager em;
+    @Autowired
+    MerchantRepository merchantRepository;
+
+    @Test
+    @DisplayName("가맹점 조회 - 성공")
+    void select_merchant() {
+        Merchant actual = new Merchant("네네치킨");
+
+        merchantRepository.save(actual);
+
+        Merchant expect = merchantRepository.findMerchantByName("네네치킨").orElseThrow();
+        assertThat(expect.getId()).isEqualTo(actual.getId());
+        assertThat(expect.getName()).isEqualTo(actual.getName());
+    }
+
+    @Test
+    @DisplayName("가맹점 저장 - 성공")
+    void create_merchant() {
+        Merchant actual = new Merchant("땅땅치킨");
+
+        Long merchantId = merchantRepository.save(actual).getId();
+
+        Merchant expect = em.find(Merchant.class, merchantId);
+
+        assertThat(expect.getId()).isEqualTo(actual.getId());
+        assertThat(expect.getName()).isEqualTo(actual.getName());
+    }
+
+    @Test
+    @DisplayName("가맹점 수정 - 성공")
+    void update_merchant() {
+        Merchant actual = new Merchant("가가치킨");
+        merchantRepository.save(actual);
+
+        actual.modifyMerchant("네네치킨");
+
+        Merchant expect = em.find(Merchant.class, actual.getId());
+
+        assertThat(expect.getId()).isEqualTo(actual.getId());
+        assertThat(expect.getName()).isEqualTo("네네치킨");
+    }
+
+    @Test
+    @DisplayName("가맹점 존재 여부 - 성공")
+    void exist_merchant() {
+        Merchant actual = new Merchant("땅땅치킨");
+        merchantRepository.save(actual);
+
+        boolean expect = merchantRepository.existsMerchantByName("땅땅치킨");
+
+        assertThat(expect).isTrue();
+    }
+}
