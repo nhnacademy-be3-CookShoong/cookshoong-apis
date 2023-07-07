d5b51b84f19474626018fc1249ec67da5d69c877
eora21 <eora21@naver.com>
2023-07-07 16:53:49 +0900

    feat: 🎸 추가 업캐스팅 쿠폰 사용처 객체를 다운캐스팅 및 사용처명을 반환하는 Converter 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/util/CouponUsageConverter.java
@@ -0,0 +1,42 @@
+package store.cookshoong.www.cookshoongbackend.coupon.util;
+
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.stereotype.Component;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsage;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageAll;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageMerchant;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageStore;
+
+/**
+ * 업캐스팅 쿠폰 사용처 객체를 다운캐스팅 및 연관관계 entity 이름을 반환하는 Converter.
+ *
+ * @author eora21
+ * @since 2023.07.06
+ */
+@Component
+public class CouponUsageConverter {
+    private static final Map<Class<? extends CouponUsage>, Function<CouponUsage, String>> COUPON_USAGE_MAPPER = Map.of(
+        CouponUsageAll.class, CouponUsageConverter::couponUsageAllToDto,
+        CouponUsageMerchant.class, CouponUsageConverter::couponUsageMerchantToDto,
+        CouponUsageStore.class, CouponUsageConverter::couponUsageStoreToDto
+    );
+    private static final String COUPON_USAGE_ALL_STRING = "모든 매장!";
+
+    private static String couponUsageAllToDto(CouponUsage couponUsageAll) {
+        return COUPON_USAGE_ALL_STRING;
+    }
+
+    private static String couponUsageMerchantToDto(CouponUsage couponUsageMerchant) {
+        return ((CouponUsageMerchant) couponUsageMerchant).getMerchant().getName();
+    }
+
+    private static String couponUsageStoreToDto(CouponUsage couponUsageStore) {
+        return ((CouponUsageStore) couponUsageStore).getStore().getName();
+    }
+
+    public String convert(CouponUsage couponUsage) {
+        return COUPON_USAGE_MAPPER.get(couponUsage.getClass())
+            .apply(couponUsage);
+    }
+}
