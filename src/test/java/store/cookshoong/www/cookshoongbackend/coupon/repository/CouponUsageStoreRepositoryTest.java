0ca9fc6fa0b215bb3ee9335edcb798cfa4638260
eora21 <eora21@naver.com>
2023-07-07 14:52:47 +0900

    test: 💍 수정 쿠폰 매장 사용처 테스트에 util 적용 및 entity 구조 변경에 따른 코드 변경

--- src/test/java/store/cookshoong/www/cookshoongbackend/coupon/repository/CouponUsageStoreRepositoryTest.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/coupon/repository/CouponUsageStoreRepositoryTest.java
@@ -1,5 +1,8 @@
 package store.cookshoong.www.cookshoongbackend.coupon.repository;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -7,12 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageStore;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import store.cookshoong.www.cookshoongbackend.store.entity.Store;
+import store.cookshoong.www.cookshoongbackend.util.TestPersistEntity;
 
 @DataJpaTest
+@Import(TestPersistEntity.class)
 class CouponUsageStoreRepositoryTest {
     @MockBean
     JPAQueryFactory jpaQueryFactory;
@@ -20,20 +24,22 @@ class CouponUsageStoreRepositoryTest {
     @Autowired
     CouponUsageStoreRepository couponUsageStoreRepository;
 
+    @Autowired
+    TestPersistEntity testPersistEntity;
+
     @Test
     @DisplayName("매장 id로 쿠폰 매장 사용처 획득 - 성공")
     void findByStoreIdSuccessTest() throws Exception {
-        Long storeId = 3L;
-        couponUsageStoreRepository.saveAndFlush(new CouponUsageStore(storeId));
-
-        assertDoesNotThrow(() -> couponUsageStoreRepository.findByStoreId(storeId)
+        Store store = testPersistEntity.getOpenStore();
+        couponUsageStoreRepository.saveAndFlush(new CouponUsageStore(store));
+        assertDoesNotThrow(() -> couponUsageStoreRepository.findByStoreId(store.getId())
             .orElseThrow(IllegalArgumentException::new));
     }
 
     @Test
     @DisplayName("매장 id로 쿠폰 매장 사용처 획득 - 실패")
     void findByStoreIdFailTest() throws Exception {
-        Long storeId = 3L;
+        Long storeId = Long.MIN_VALUE;
         Optional<CouponUsageStore> byStoreId = couponUsageStoreRepository.findByStoreId(storeId);
 
         assertThrows(IllegalArgumentException.class,
