da9a93fb6e328270998cab59e7665addb6eeb023
eora21 <eora21@naver.com>
2023-07-07 15:16:41 +0900

    test: 💍 수정 쿠폰 타입 금액 테스트에 TestEntity 객체 적용

--- src/test/java/store/cookshoong/www/cookshoongbackend/coupon/repository/CouponTypeCashRepositoryTest.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/coupon/repository/CouponTypeCashRepositoryTest.java
@@ -1,5 +1,8 @@
 package store.cookshoong.www.cookshoongbackend.coupon.repository;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -8,9 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypeCash;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import store.cookshoong.www.cookshoongbackend.util.TestEntity;
 
 @DataJpaTest
 class CouponTypeCashRepositoryTest {
@@ -20,25 +21,25 @@ class CouponTypeCashRepositoryTest {
     @Autowired
     CouponTypeCashRepository couponTypeCashRepository;
 
-    int discountAmount = 1_000;
-
-    int minimumPrice = 10_000;
-
     @Test
     @DisplayName("할인금액 및 최소금액으로 쿠폰 타입 획득 - 성공")
     void findByDiscountAmountAndMinimumPriceSuccess() throws Exception {
-        couponTypeCashRepository.saveAndFlush(new CouponTypeCash(discountAmount, minimumPrice));
+        CouponTypeCash couponTypeCash = TestEntity.COUPON_TYPE_CASH_1000_10000;
+        couponTypeCashRepository.saveAndFlush(couponTypeCash);
 
         assertDoesNotThrow(() ->
-            couponTypeCashRepository.findByDiscountAmountAndMinimumPrice(discountAmount, minimumPrice))
+            couponTypeCashRepository.findByDiscountAmountAndMinimumPrice(
+                couponTypeCash.getDiscountAmount(), couponTypeCash.getMinimumPrice()))
             .orElseThrow(IllegalArgumentException::new);
     }
 
     @Test
     @DisplayName("할인금액 및 최소금액으로 쿠폰 타입 획득 - 실패")
     void findByDiscountAmountAndMinimumPriceFail() throws Exception {
+        CouponTypeCash couponTypeCash = TestEntity.COUPON_TYPE_CASH_1000_10000;
         Optional<CouponTypeCash> byDiscountAmountAndMinimumPrice =
-            couponTypeCashRepository.findByDiscountAmountAndMinimumPrice(discountAmount, minimumPrice);
+            couponTypeCashRepository.findByDiscountAmountAndMinimumPrice(
+                couponTypeCash.getDiscountAmount(), couponTypeCash.getMinimumPrice());
 
         assertThrows(IllegalArgumentException.class,
             () -> byDiscountAmountAndMinimumPrice.orElseThrow(IllegalArgumentException::new));
