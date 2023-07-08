04c03434de7eb100ec927769180ffffe45cb5241
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-08 18:02:17 +0900

    refactor: :bulb: 수정 Store관련 Dto QueryProjection 추가
    QueryProjection으로 Querydsl 사용하기 위해 추가함

--- src/main/java/store/cookshoong/www/cookshoongbackend/store/model/response/SelectAllStoresResponseDto.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/store/model/response/SelectAllStoresResponseDto.java
@@ -1,5 +1,6 @@
 package store.cookshoong.www.cookshoongbackend.store.model.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 /**
@@ -10,10 +11,31 @@ import lombok.Getter;
  */
 @Getter
 public class SelectAllStoresResponseDto {
-    private Long storeId;
-    private String loginId;
-    private String storeName;
-    private String storeMainAddress;
-    private String storeDetailAddress;
-    private String storeStatus;
+    private final Long storeId;
+    private final String loginId;
+    private final String storeName;
+    private final String storeMainAddress;
+    private final String storeDetailAddress;
+    private final String storeStatus;
+
+    /**
+     * 사업자 회원 : 사업자 회원이 소유한 매장 리스트.
+     *
+     * @param storeId            매장 아이디
+     * @param loginId            사업자 회원의 로그인 아이디
+     * @param storeName          매장 이름
+     * @param storeMainAddress   매장의 메인주소
+     * @param storeDetailAddress 매장의 상세주소
+     * @param storeStatus        매장 상태
+     */
+    @QueryProjection
+    public SelectAllStoresResponseDto(Long storeId, String loginId, String storeName, String storeMainAddress,
+                                      String storeDetailAddress, String storeStatus) {
+        this.storeId = storeId;
+        this.loginId = loginId;
+        this.storeName = storeName;
+        this.storeMainAddress = storeMainAddress;
+        this.storeDetailAddress = storeDetailAddress;
+        this.storeStatus = storeStatus;
+    }
 }
