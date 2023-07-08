00393e41cbacc0a538f84d55a2841a6110df7650
eora21 <eora21@naver.com>
2023-07-08 22:37:11 +0900

    refactor: 💡 수정 TestPersistEntity에서 AOP 활성화 및 static 해제

--- src/test/java/store/cookshoong/www/cookshoongbackend/util/TestPersistEntity.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/util/TestPersistEntity.java
@@ -1,41 +1,53 @@
 package store.cookshoong.www.cookshoongbackend.util;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestComponent;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import store.cookshoong.www.cookshoongbackend.account.entity.Account;
 import store.cookshoong.www.cookshoongbackend.account.entity.AccountsStatus;
 import store.cookshoong.www.cookshoongbackend.account.entity.Authority;
 import store.cookshoong.www.cookshoongbackend.account.entity.Rank;
-import store.cookshoong.www.cookshoongbackend.address.entity.Address;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypeCash;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageStore;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
 import store.cookshoong.www.cookshoongbackend.store.entity.BankType;
 import store.cookshoong.www.cookshoongbackend.store.entity.Store;
 import store.cookshoong.www.cookshoongbackend.store.entity.StoreStatus;
 
 /**
  * TestEntity 내 미리 만들어 둔 entity 객체들이 필요한 타 객체들을 미리 em.persist 하여 쉽게 사용할 수 있도록 메서드를 모아두는 클래스.
- * 만약 DataJpaTest 내에서 사용 시 `@Import(TestPersistEntity.class)` 구문을 추가해주세요.
  *
  * @author eora21
  * @since 2023.07.07
  */
 @TestComponent
+@EnableAspectJAutoProxy
+@Import(TestEntity.class)
 public class TestPersistEntity {
     @Autowired
-    TestEntityManager em;
+    TestEntity testEntity;
 
-    public Account getLevelOneCustomer() {
-        AccountsStatus accountsStatus = em.persist(TestEntity.getAccountStatusActive());
-        Authority authority = em.persist(TestEntity.getAuthorityCustomer());
-        Rank rank = em.persist(TestEntity.getRankLevelOne());
-        return em.persist(TestEntity.getAccount(accountsStatus, authority, rank));
+    public Account getLevelOneActiveCustomer() {
+        AccountsStatus accountStatusActive = testEntity.getAccountStatusActive();
+        Authority authorityCustomer = testEntity.getAuthorityCustomer();
+        Rank rankLevelOne = testEntity.getRankLevelOne();
+        return testEntity.getAccount(accountStatusActive, authorityCustomer, rankLevelOne);
     }
 
     public Store getOpenStore() {
-        Account account = getLevelOneCustomer();
-        BankType bankType = em.persist(TestEntity.getBankTypeKb());
-        StoreStatus storeStatus = em.persist(TestEntity.getStoreStatusOpen());
-        return em.persist(TestEntity.getStore(account, bankType, storeStatus));
+        Account account = getLevelOneActiveCustomer();
+        BankType bankTypeKb = testEntity.getBankTypeKb();
+        StoreStatus storeStatusOpen = testEntity.getStoreStatusOpen();
+        return testEntity.getStore(account, bankTypeKb, storeStatusOpen);
+    }
+
+    public IssueCoupon getIssueCoupon() {
+        Store store = getOpenStore();
+        CouponUsageStore couponUsageStore = testEntity.getCouponUsageStore(store);
+        CouponTypeCash couponTypeCash = testEntity.getCouponTypeCash_1000_10000();
+        CouponPolicy couponPolicy = testEntity.getCouponPolicy(couponTypeCash, couponUsageStore);
+        return testEntity.getIssueCoupon(couponPolicy);
     }
 }
