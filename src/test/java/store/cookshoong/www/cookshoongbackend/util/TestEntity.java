71cabf8fdc1bfef0d5fad2d11c5f61f349d2ca02
eora21 <eora21@naver.com>
2023-07-08 22:34:22 +0900

    refactor: 💡 수정 AOP 적용을 위해 TestEntity의 모든 메서드 static 해제

--- src/test/java/store/cookshoong/www/cookshoongbackend/util/TestEntity.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/util/TestEntity.java
@@ -2,15 +2,30 @@ package store.cookshoong.www.cookshoongbackend.util;
 
 import java.lang.reflect.Constructor;
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.util.ReflectionTestUtils;
 import store.cookshoong.www.cookshoongbackend.account.entity.Account;
 import store.cookshoong.www.cookshoongbackend.account.entity.AccountsStatus;
 import store.cookshoong.www.cookshoongbackend.account.entity.Authority;
 import store.cookshoong.www.cookshoongbackend.account.entity.Rank;
 import store.cookshoong.www.cookshoongbackend.address.entity.Address;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponLog;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponLogType;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponType;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypeCash;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponTypePercent;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsage;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponUsageStore;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
+import store.cookshoong.www.cookshoongbackend.payment.entity.Order;
 import store.cookshoong.www.cookshoongbackend.store.entity.BankType;
 import store.cookshoong.www.cookshoongbackend.store.entity.Holiday;
 import store.cookshoong.www.cookshoongbackend.store.entity.Store;
@@ -23,71 +38,117 @@ import store.cookshoong.www.cookshoongbackend.store.entity.StoreStatus;
  * @author eora21
  * @since 2023.07.07
  */
+@TestComponent
+@Import(TestEntityAspect.class)
 public class TestEntity {
-    public static Address getAddress() {
+    public Address getAddress() {
         return new Address("main", "detail", BigDecimal.ONE, BigDecimal.ZERO);
     }
 
-    public static AccountsStatus getAccountStatusActive() {
+    public AccountsStatus getAccountStatusActive() {
         return new AccountsStatus("ACTIVE", "활성");
     }
 
-    public static Authority getAuthorityCustomer() {
+    public Authority getAuthorityCustomer() {
         return new Authority("CUSTOMER", "일반회원");
     }
 
-    public static Rank getRankLevelOne() {
+    public Rank getRankLevelOne() {
         return new Rank("LEVEL_1", "프렌드");
     }
 
-    public static Account getAccount(AccountsStatus accountsStatus, Authority authority, Rank rank) {
+    public Account getAccount(AccountsStatus accountsStatus, Authority authority, Rank rank) {
         return new Account(accountsStatus, authority, rank, "eora21", "pwd", "김주호",
             "말비묵", "test@test.com", LocalDate.of(1996, 4, 1),
             "01012345678");
     }
 
-    public static BankType getBankTypeKb() {
+    public BankType getBankTypeKb() {
         return createTestBankType("KB", "국민은행");
     }
 
-    public static StoreStatus getStoreStatusOpen() {
+    public StoreStatus getStoreStatusOpen() {
         return createTestStoreStatus("OPEN", "영업중");
     }
 
-    public static Store getStore(Account account, BankType bankType, StoreStatus storeStatus) {
+    public Store getStore(Account account, BankType bankType, StoreStatus storeStatus) {
         return new Store(null, account, bankType,
             storeStatus, "license", "123456", "김주호",
             LocalDate.of(2020, 2, 20), "주호타코", "01012345678", BigDecimal.ONE,
             null, null, "123456");
     }
 
-    public static CouponTypePercent getCouponTypePercent_3_1000_10000() {
+    public CouponTypePercent getCouponTypePercent_3_1000_10000() {
         return new CouponTypePercent(new BigDecimal("3"), 1_000, 10_000);
     }
 
-    public static CouponTypeCash getCouponTypeCash_1000_10000() {
+    public CouponTypeCash getCouponTypeCash_1000_10000() {
         return new CouponTypeCash(1_000, 10_000);
     }
 
-    public static Holiday getHoliday(Store store) {
+    public CouponUsageStore getCouponUsageStore(Store store) {
+        return new CouponUsageStore(store);
+    }
+
+    public CouponPolicy getCouponPolicy(CouponType couponType, CouponUsage couponUsage) {
+        return new CouponPolicy(couponType, couponUsage, "testCoupon", "just Test",
+            LocalTime.of(1, 0, 0));
+    }
+
+    public IssueCoupon getIssueCoupon(CouponPolicy couponPolicy) {
+        LocalTime expirationTime = couponPolicy.getExpirationTime();
+        return new IssueCoupon(couponPolicy,
+            LocalDateTime.now()
+                .plusHours(expirationTime.getHour())
+                .plusMinutes(expirationTime.getMinute())
+                .plusSeconds(expirationTime.getSecond()));
+    }
+
+    public CouponLogType getCouponLogType() {
+        return createTestCouponLogType();
+    }
+
+    public CouponLog getCouponLog(IssueCoupon issueCoupon, CouponLogType couponLogType, Order order) {
+        return new CouponLog(issueCoupon, couponLogType, order, LocalDateTime.now());
+    }
+
+    public Holiday getHoliday(Store store) {
         return new Holiday(store, LocalDate.of(2020, 2, 20));
     }
 
-    private static BankType createTestBankType(String bankTypeCode, String description) {
+    public Order getOrder() {
+        return createTestOrder();
+    }
+
+    private BankType createTestBankType(String bankTypeCode, String description) {
         BankType bankType = createEntityUsingDeclared(BankType.class);
         ReflectionTestUtils.setField(bankType, "bankTypeCode", bankTypeCode);
         ReflectionTestUtils.setField(bankType, "description", description);
         return bankType;
     }
 
-    private static StoreStatus createTestStoreStatus(String storeStatusCode, String description) {
+    private StoreStatus createTestStoreStatus(String storeStatusCode, String description) {
         StoreStatus storeStatus = createEntityUsingDeclared(StoreStatus.class);
         ReflectionTestUtils.setField(storeStatus, "storeStatusCode", storeStatusCode);
         ReflectionTestUtils.setField(storeStatus, "description", description);
         return storeStatus;
     }
 
-    private static <T> T createEntityUsingDeclared(Class<T> clazz) {
+    private CouponLogType createTestCouponLogType() {
+        CouponLogType couponLogType = createEntityUsingDeclared(CouponLogType.class);
+        ReflectionTestUtils.setField(couponLogType, "name", "USE");
+        return couponLogType;
+    }
+
+    private Order createTestOrder() {
+        Order order = createEntityUsingDeclared(Order.class);
+        ReflectionTestUtils.setField(order, "orderCode", UUID.randomUUID());
+        ReflectionTestUtils.setField(order, "orderedAt", LocalDateTime.now());
+        ReflectionTestUtils.setField(order, "memo", "memo");
+        return order;
+    }
+
+    private <T> T createEntityUsingDeclared(Class<T> clazz) {
         try {
             Constructor<T> constructor = clazz.getDeclaredConstructor();
             constructor.setAccessible(true);
