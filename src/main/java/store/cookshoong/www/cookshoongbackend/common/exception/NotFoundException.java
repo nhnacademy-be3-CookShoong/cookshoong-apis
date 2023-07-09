3832366c290747e4085b90aa15f0b277a85829ab
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-09 17:26:19 +0900

    feat: 🎸 수정 NotFoundException 메세지 입력방식 변경
    기존에 어떤 값이 없다는 것을 알려주는 것에서 사용자에게 불필요한 정보를 주는 것 같아 무슨 종류가 없는지에 대한 것으로 변경하였습니다.
    
    추가로 에러메세지만을 담아주는 클래스를 작성하였습니다.

--- src/main/java/store/cookshoong/www/cookshoongbackend/common/exception/NotFoundException.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/common/exception/NotFoundException.java
@@ -1,5 +1,6 @@
 package store.cookshoong.www.cookshoongbackend.common.exception;
 
+
 /**
  * 원하는 객체를 찾지 못했을 때의 예외.
  *
@@ -7,7 +8,7 @@ package store.cookshoong.www.cookshoongbackend.common.exception;
  * @since 2023.07.06
  */
 public abstract class NotFoundException extends RuntimeException {
-    protected NotFoundException(String message) {
-        super(message);
+    protected NotFoundException(String target) {
+        super("존재하지 않는 " + target + "입니다");
     }
 }
