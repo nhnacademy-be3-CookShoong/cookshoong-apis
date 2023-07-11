8d7162765767d09582f334e5903cb91b4ad22317
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-11 16:36:41 +0900

    refactor: 💡 수정 DB 정보 호출 추상화 적용
    SKM을 통해 DB 정보를 가져오는 메소드를 추상화 시켰습니다.

--- src/main/java/store/cookshoong/www/cookshoongbackend/config/DatabaseConfig.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/config/DatabaseConfig.java
@@ -1,37 +1,16 @@
 package store.cookshoong.www.cookshoongbackend.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.util.Map;
-import java.util.Objects;
-import javax.net.ssl.SSLContext;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-import store.cookshoong.www.cookshoongbackend.common.model.response.SecureKeyManagerResponseDto;
 import store.cookshoong.www.cookshoongbackend.common.property.DatabaseProperties;
-import store.cookshoong.www.cookshoongbackend.common.property.SecureKeyManagerProperties;
+import store.cookshoong.www.cookshoongbackend.common.service.SKMService;
 
 /**
  * DB 설정에 대한 Configuration Class.
@@ -43,7 +22,7 @@ import store.cookshoong.www.cookshoongbackend.common.property.SecureKeyManagerPr
 @Configuration
 @RequiredArgsConstructor
 public class DatabaseConfig {
-    private final RestTemplate restTemplate;
+    private final SKMService skmService;
 
     /**
      * DB 설정을 마친 Datasource.
@@ -68,63 +47,17 @@ public class DatabaseConfig {
     }
 
     /**
-     * SKM 로 부터 인증서를 보내 DB 설정값들을 가져온다.
+     * SKM 로 부터 클라이언트 인증서를 보내 DB 설정값들을 가져온다.
      *
-     * @param secureKeyManagerProperties SKM 인증정보
+     * @param mysqlKeyid SKM 저장되있는 기밀 데이터의 아이디
      * @return DB 설정값
-     * @throws KeyStoreException         the key store exception
-     * @throws CertificateException      the certificate exception
-     * @throws IOException               the io exception
-     * @throws NoSuchAlgorithmException  the no such algorithm exception
-     * @throws UnrecoverableKeyException the unrecoverable key exception
-     * @throws KeyManagementException    the key management exception
+     * @throws JsonProcessingException the json processing exception
      */
     @Bean
     @Profile("!default")
-    public DatabaseProperties databaseProperties(SecureKeyManagerProperties secureKeyManagerProperties)
-        throws KeyStoreException, CertificateException, IOException,
-        NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
-
-        String password = secureKeyManagerProperties.getPassword();
-        String keyid = secureKeyManagerProperties.getMysqlKeyid();
-        String appkey = secureKeyManagerProperties.getAppkey();
-
-        KeyStore clientStore = KeyStore.getInstance("PKCS12");
-        InputStream inputStream = new ClassPathResource("CookShoong.p12").getInputStream();
-        clientStore.load(inputStream, password.toCharArray());
-        SSLContext sslContext = SSLContextBuilder.create()
-            .setProtocol("TLS")
-            .loadKeyMaterial(clientStore, password.toCharArray())
-            .loadTrustMaterial(new TrustSelfSignedStrategy())
-            .build();
-
-        SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom()
-            .setSSLSocketFactory(sslConnectionSocketFactory)
-            .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        restTemplate.setRequestFactory(requestFactory);
-
-        URI uri = UriComponentsBuilder
-            .fromUriString("https://api-keymanager.nhncloudservice.com/keymanager/v1.0/appkey/{appkey}/secrets/{keyid}")
-            .uriVariables(Map.of("appkey", appkey, "keyid", keyid))
-            .encode()
-            .build()
-            .toUri();
-
-        String response = Objects.requireNonNull(restTemplate
-                .getForEntity(uri, SecureKeyManagerResponseDto.class).getBody()
-            )
-            .getResponseBody()
-            .getSecrets();
-
-        return extractDatabasePropertiesFrom(response);
-    }
-
-    private  DatabaseProperties extractDatabasePropertiesFrom(String response) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(response, DatabaseProperties.class);
+    public DatabaseProperties databaseProperties(@Value("${cookshoong.skm.keyid.mysql}") String mysqlKeyid)
+        throws JsonProcessingException {
+        return skmService.fetchSecrets(mysqlKeyid, DatabaseProperties.class);
     }
 }
 
