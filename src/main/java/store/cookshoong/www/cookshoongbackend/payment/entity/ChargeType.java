9f531458f243bf3cc4ae57d67a10ae57afb87759
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-11 15:20:50 +0900

    refactor: :bulb: 수정 ERD에 맞도록 Charge, Refund Entity 재작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/payment/entity/ChargeType.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/payment/entity/ChargeType.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import store.cookshoong.www.cookshoongbackend.payment.model.request.ModifyTypeRequestDto;
@@ -23,20 +24,24 @@ import store.cookshoong.www.cookshoongbackend.payment.model.request.ModifyTypeRe
 @Table(name = "charge_types")
 public class ChargeType {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "charge_type_id", nullable = false)
-    private Long id;
+    @Column(name = "charge_type_code", nullable = false)
+    private String id;
 
     @Column(name = "name", nullable = false, length = 30)
     private String name;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
 
     /**
      * 결제 타입 name 에 대한 생성자.
      *
      * @param name      결제 타입 이름
      */
-    public ChargeType(String name) {
+    public ChargeType(String id, String name, boolean isDeleted) {
+        this.id = id;
         this.name = name;
+        this.isDeleted = isDeleted;
     }
 
     /**
--- src/main/java/store/cookshoong/www/cookshoongbackend/payment/entity/RefundType.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/payment/entity/RefundType.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import store.cookshoong.www.cookshoongbackend.payment.model.request.ModifyTypeRequestDto;
@@ -24,20 +25,24 @@ import store.cookshoong.www.cookshoongbackend.payment.model.request.ModifyTypeRe
 public class RefundType {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "refund_type_id", nullable = false)
-    private Long id;
+    @Column(name = "refund_type_code", nullable = false)
+    private String id;
 
     @Column(name = "name", nullable = false, length = 30)
     private String name;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
 
     /**
      * 환불 타입 name 에 대한 생성.
      *
      * @param name      환불 타입 이름
      */
-    public RefundType(String name) {
+    public RefundType(String id, String name, boolean isDeleted) {
+        this.id = id;
         this.name = name;
+        this.isDeleted = isDeleted;
     }
 
     /**
