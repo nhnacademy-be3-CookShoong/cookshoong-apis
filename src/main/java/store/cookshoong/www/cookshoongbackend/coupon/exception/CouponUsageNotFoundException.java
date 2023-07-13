90fe70e4aacc46fde9e9cf7ebe24984d3330abb5
eora21 <eora21@naver.com>
2023-07-13 15:48:37 +0900

    feat: 🎸 추가 쿠폰 사용처 값 찾지 못했을 때 발생하는 예외 정의

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/exception/CouponUsageNotFoundException.java
@@ -0,0 +1,15 @@
+package store.cookshoong.www.cookshoongbackend.coupon.exception;
+
+import store.cookshoong.www.cookshoongbackend.common.exception.NotFoundException;
+
+/**
+ * 쿠폰 사용처를 찾을 수 없을 때 발생되는 예외.
+ *
+ * @author eora21(김주호)
+ * @since 2023.07.13
+ */
+public class CouponUsageNotFoundException extends NotFoundException {
+    public CouponUsageNotFoundException() {
+        super("쿠폰 사용처를 찾을 수 없습니다.");
+    }
+}
