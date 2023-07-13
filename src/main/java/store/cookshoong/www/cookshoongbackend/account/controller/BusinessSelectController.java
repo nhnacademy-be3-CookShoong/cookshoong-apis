293fd8d4954f9c51a6f47acf8c2d02c2fb6995b3
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-13 13:27:17 +0900

    docs: :pencil2:  추가 자바독 미작성 된 부분 추가(사업자 페이지 리스트 조회 컨트롤러)

--- src/main/java/store/cookshoong/www/cookshoongbackend/account/controller/BusinessSelectController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/account/controller/BusinessSelectController.java
@@ -6,12 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import store.cookshoong.www.cookshoongbackend.store.model.response.SelectAllBanksForUserResponseDto;
-import store.cookshoong.www.cookshoongbackend.store.model.response.SelectAllCategoriesForUserResponseDto;
-import store.cookshoong.www.cookshoongbackend.store.model.response.SelectAllMerchantsForUserResponseDto;
-import store.cookshoong.www.cookshoongbackend.store.service.BankTypeService;
-import store.cookshoong.www.cookshoongbackend.store.service.MerchantService;
-import store.cookshoong.www.cookshoongbackend.store.service.StoreCategoryService;
+import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllBanksForUserResponseDto;
+import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllCategoriesForUserResponseDto;
+import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllMerchantsForUserResponseDto;
+import store.cookshoong.www.cookshoongbackend.shop.service.BankTypeService;
+import store.cookshoong.www.cookshoongbackend.shop.service.MerchantService;
+import store.cookshoong.www.cookshoongbackend.shop.service.StoreCategoryService;
 
 /**
  * 사업자 : 등록, 수정시 필요한 selectBox, checkBox 용 api.
@@ -28,18 +28,33 @@ public class BusinessSelectController {
     private final MerchantService merchantService;
     private final StoreCategoryService storeCategoryService;
 
+    /**
+     * 사업자등록 : 은행 리스트 조회.
+     *
+     * @return 은행 리스트
+     */
     @GetMapping("/banks")
     public ResponseEntity<List<SelectAllBanksForUserResponseDto>> getBanksForUser() {
         return ResponseEntity
             .ok(bankTypeService.selectBanksForUser());
     }
 
+    /**
+     * 사업자 등록 : 가맹점 리스트 조회.
+     *
+     * @return 가맹점 리스트
+     */
     @GetMapping("/merchants")
     public ResponseEntity<List<SelectAllMerchantsForUserResponseDto>> getMerchantsForUser() {
         return ResponseEntity
             .ok(merchantService.selectAllMerchantsForUser());
     }
 
+    /**
+     * 사업자 등록 : 카테고리 리스트 조회.
+     *
+     * @return 매장 키테고리 리스트
+     */
     @GetMapping("/categories")
     public ResponseEntity<List<SelectAllCategoriesForUserResponseDto>> getCategoriesForUser() {
         return ResponseEntity
