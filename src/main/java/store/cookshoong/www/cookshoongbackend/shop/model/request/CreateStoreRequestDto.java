79d70eb92094b621b77ba23fef351ed4ab3584ab
윤동현 <eatpaper1429@gmail.com>
2023-07-14 10:35:03 +0900

    feat: 🎸 추가 Dto validation 추가

--- src/main/java/store/cookshoong/www/cookshoongbackend/shop/model/request/CreateStoreRequestDto.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/shop/model/request/CreateStoreRequestDto.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * 매장 등록 Dto.
@@ -17,24 +19,34 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateStoreRequestDto {
-    //TODO 2. validation 재작성
     private String merchantName;
     @NotBlank
     private String businessLicenseNumber;
+    @NotBlank
     private String representativeName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate openingDate;
+    @NotBlank
     private String storeName;
+    @NotBlank
     private String mainPlace;
     private String detailPlace;
+    @NotNull
     private BigDecimal latitude;
+    @NotNull
     private BigDecimal longitude;
+    @NotBlank
     private String phoneNumber;
+    @NotBlank
     private String businessLicense;
     private String description;
+    @NotNull
     private BigDecimal earningRate;
     private String image;
+    @NotNull
     private List<String> storeCategories;
+    @NotBlank
     private String bankName;
+    @NotBlank
     private String bankAccount;
-
 }
