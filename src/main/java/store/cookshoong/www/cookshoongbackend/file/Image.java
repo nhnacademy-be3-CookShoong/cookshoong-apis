1635cb6927b49ce7dceef3c480389d637b1efb25
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-19 13:10:30 +0900

    feat: :guitar: 추가 이미지 테이블 추가로 인한 엔티티 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/file/Image.java
@@ -0,0 +1,34 @@
+package store.cookshoong.www.cookshoongbackend.file;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "saved_name", nullable = false, length = 40)
+    private String savedName;
+
+
+    @Column(name = "is_public", nullable = false)
+    private boolean isPublic;
+
+}
