26e0eef46391bf7871244ee2acaba7cc561631f7
eora21 <eora21@naver.com>
2023-07-20 17:39:46 +0900

    feat: 🎸 추가 쿠폰 발급 요청 시 사용되는 dto 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/model/request/CreateIssueCouponRequestDto.java
@@ -0,0 +1,18 @@
+package store.cookshoong.www.cookshoongbackend.coupon.model.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 쿠폰 발급 요청 시 사용되는 dto.
+ *
+ * @author eora21(김주호)
+ * @since 2023.07.17
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateIssueCouponRequestDto {
+    Long issueQuantity;
+    Long couponPolicyId;
+}
