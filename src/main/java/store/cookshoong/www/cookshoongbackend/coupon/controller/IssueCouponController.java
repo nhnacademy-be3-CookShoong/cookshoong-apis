e77d70e5bd7cdc3eb9d0ceec7e250ff549cf53b8
eora21 <eora21@naver.com>
2023-07-20 18:40:03 +0900

    refactor: 💡 (IssueCouponController) 수정 created 반환, javadoc 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/controller/IssueCouponController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/controller/IssueCouponController.java
@@ -1,6 +1,8 @@
 package store.cookshoong.www.cookshoongbackend.coupon.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,7 @@ import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreateIssueCo
 import store.cookshoong.www.cookshoongbackend.coupon.service.IssueCouponService;
 
 /**
- * 쿠폰 발급 엔드포인트.
+ * 쿠폰 발행 RestController.
  *
  * @author eora21(김주호)
  * @since 2023.07.17
@@ -20,8 +22,17 @@ import store.cookshoong.www.cookshoongbackend.coupon.service.IssueCouponService;
 public class IssueCouponController {
     private final IssueCouponService issueCouponService;
 
+    /**
+     * 쿠폰 발행 엔드포인트.
+     *
+     * @param createIssueCouponRequestDto 쿠폰 정책 id와 발행 개수를 담은 dto
+     * @return Http Status Created
+     */
     @PostMapping
-    public void postIssueCoupon(@RequestBody CreateIssueCouponRequestDto createIssueCouponRequestDto) {
+    public ResponseEntity<Void> postIssueCoupon(@RequestBody CreateIssueCouponRequestDto createIssueCouponRequestDto) {
         issueCouponService.createIssueCoupon(createIssueCouponRequestDto);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .build();
     }
 }
