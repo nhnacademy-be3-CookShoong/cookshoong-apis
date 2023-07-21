f20319c3102a29bfdac53d40f6d5fadebcf99836
eora21 <eora21@naver.com>
2023-07-21 12:54:21 +0900

    feat: 🎸 추가 쿠폰 발행 엔드포인트에 BindingResult 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/controller/IssueCouponController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/controller/IssueCouponController.java
@@ -1,12 +1,16 @@
 package store.cookshoong.www.cookshoongbackend.coupon.controller;
 
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponPolicyRequestValidationException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponRequestValidationException;
 import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreateIssueCouponRequestDto;
 import store.cookshoong.www.cookshoongbackend.coupon.service.IssueCouponService;
 
@@ -29,7 +33,14 @@ public class IssueCouponController {
      * @return Http Status Created
      */
     @PostMapping
-    public ResponseEntity<Void> postIssueCoupon(@RequestBody CreateIssueCouponRequestDto createIssueCouponRequestDto) {
+    public ResponseEntity<Void> postIssueCoupon(@RequestBody @Valid
+                                                    CreateIssueCouponRequestDto createIssueCouponRequestDto,
+                                                BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new IssueCouponRequestValidationException(bindingResult);
+        }
+
         issueCouponService.createIssueCoupon(createIssueCouponRequestDto);
         return ResponseEntity
             .status(HttpStatus.CREATED)
