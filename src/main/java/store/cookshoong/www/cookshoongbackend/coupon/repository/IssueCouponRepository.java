d46c19294ecbc06a4387f1fc5684349a33cc2443
eora21 <eora21@naver.com>
2023-07-23 15:36:47 +0900

    feat: 🎸 추가 유저에게 발급되지 않은 쿠폰들을 조회하는 메서드 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
@@ -1,6 +1,9 @@
 package store.cookshoong.www.cookshoongbackend.coupon.repository;
 
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
 
 /**
@@ -9,6 +12,6 @@ import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
  * @author eora21
  * @since 2023.07.04
  */
-public interface IssueCouponRepository extends JpaRepository<IssueCoupon, String>, IssueCouponRepositoryCustom {
-
+public interface IssueCouponRepository extends JpaRepository<IssueCoupon, UUID>, IssueCouponRepositoryCustom {
+    List<IssueCoupon> findTop10ByCouponPolicyAndAccountIsNull(CouponPolicy couponPolicy);
 }
