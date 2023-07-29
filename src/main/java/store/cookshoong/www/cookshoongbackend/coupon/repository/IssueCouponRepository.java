96cd16de0ac9176611dc54267a27d9175b2c1f2e
eora21 <eora21@naver.com>
2023-07-29 22:16:51 +0900

    feat: 🎸 추가 사용자에게 발급되지 않은 쿠폰을 1개만 찾아오는 메서드 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
@@ -1,6 +1,7 @@
 package store.cookshoong.www.cookshoongbackend.coupon.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
@@ -20,4 +21,12 @@ public interface IssueCouponRepository extends JpaRepository<IssueCoupon, UUID>,
      * @return the list
      */
     List<IssueCoupon> findTop10ByCouponPolicyAndAccountIsNull(CouponPolicy couponPolicy);
+
+    /**
+     * 사용자에게 발급되지 않은 쿠폰을 1개만 찾아오는 메서드.
+     *
+     * @param couponPolicy the coupon policy
+     * @return the optional
+     */
+    Optional<IssueCoupon> findByCouponPolicyAndAccountIsNull(CouponPolicy couponPolicy);
 }
