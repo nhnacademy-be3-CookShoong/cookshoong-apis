9a65b05ab5f7174ddaf47704a1aeb902d73f3a05
eora21 <eora21@naver.com>
2023-07-29 22:26:36 +0900

    feat: 🎸 추가 rabbitMq에서 메시지를 수신하여 컨트롤러처럼 작동하는 컴포넌트 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/rabbitmq/controller/RabbitMqController.java
@@ -0,0 +1,45 @@
+package store.cookshoong.www.cookshoongbackend.rabbitmq.controller;
+
+import static store.cookshoong.www.cookshoongbackend.config.RabbitMqConfig.DEAD_LETTER_QUEUE_NAME;
+import static store.cookshoong.www.cookshoongbackend.config.RabbitMqConfig.QUEUE_NAME;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import store.cookshoong.www.cookshoongbackend.coupon.model.request.UpdateProvideCouponRequest;
+import store.cookshoong.www.cookshoongbackend.coupon.service.IssueCouponService;
+
+/**
+ * rabbitMq 통해 메시지를 수신하는 컴포넌트.
+ *
+ * @author eora21 (김주호)
+ * @since 2023.07.28
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMqController {
+    private final IssueCouponService issueCouponService;
+
+    /**
+     * 큐에서 메시지를 수신하여 서비스를 수행.
+     *
+     * @param updateProvideCouponRequest the update provide coupon request
+     */
+    @RabbitListener(queues = QUEUE_NAME, containerFactory = "simpleRabbitListenerContainerFactory")
+    public void receiveMessageFromQueue(UpdateProvideCouponRequest updateProvideCouponRequest) {
+        issueCouponService.provideCouponToAccountByEvent(updateProvideCouponRequest);
+    }
+
+    /**
+     * 처리하지 못 한 메시지를 수신하여 로그를 작성.
+     *
+     * @param message the message
+     */
+    @RabbitListener(queues = DEAD_LETTER_QUEUE_NAME)
+    public void processFailedMessages(Message message) {
+        log.error("dead letter message: {}", message.toString());
+    }
+}
