08f1fdee23980fbb01e1d39b48b492120024873f
eora21 <eora21@naver.com>
2023-07-29 22:22:10 +0900

    feat: 🎸 추가 분산락을 이용하여 쿠폰을 발급하는 메서드 작성
    분산락을 이용한다면 기존의 redis로 UUID를 빼오는 방법을 생각하지 않아도 되므로 메서드에서 제거

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/IssueCouponService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/IssueCouponService.java
@@ -4,23 +4,24 @@ import java.time.LocalDate;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import store.cookshoong.www.cookshoongbackend.account.entity.Account;
+import store.cookshoong.www.cookshoongbackend.account.exception.UserNotFoundException;
 import store.cookshoong.www.cookshoongbackend.account.repository.AccountRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.AlreadyHasCouponWithinSamePolicyException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponExhaustionException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponPolicyNotFoundException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponNotFoundException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponOverCountException;
@@ -30,6 +31,8 @@ import store.cookshoong.www.cookshoongbackend.coupon.model.request.UpdateProvide
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponPolicyRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.IssueCouponRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.util.IssueMethod;
+import store.cookshoong.www.cookshoongbackend.rabbitmq.exception.LockInterruptedException;
+import store.cookshoong.www.cookshoongbackend.rabbitmq.exception.LockOverWaitTimeException;
 
 /**
  * 쿠폰 발급 서비스.
@@ -42,13 +45,15 @@ import store.cookshoong.www.cookshoongbackend.coupon.util.IssueMethod;
 @RequiredArgsConstructor
 public class IssueCouponService {
     private static final int LIMIT_COUNT = 1_000;
+    private static final int WAIT_TIME = 60;
+    private static final int LEASE_TIME = 5;
 
     private final Map<IssueMethod, BiConsumer<CouponPolicy, Long>> issueMethodConsumer = createIssueConsumer();
 
     private final IssueCouponRepository issueCouponRepository;
     private final CouponPolicyRepository couponPolicyRepository;
     private final AccountRepository accountRepository;
-    private final RedisTemplate<String, Object> couponRedisTemplate;
+    private final RedissonClient redissonClient;
 
     private Map<IssueMethod, BiConsumer<CouponPolicy, Long>> createIssueConsumer() {
         Map<IssueMethod, BiConsumer<CouponPolicy, Long>> enumMap = new EnumMap<>(IssueMethod.class);
@@ -87,23 +92,7 @@ public class IssueCouponService {
             .limit(issueQuantity)
             .collect(Collectors.toSet());
 
-        issueCouponRepository.saveAllAndFlush(issueCoupons);
-
-        bulkInsertCouponCodeToRedis(issueCoupons, String.valueOf(couponPolicy.getId()));
-    }
-
-    private void bulkInsertCouponCodeToRedis(Set<IssueCoupon> issueCoupons, String couponPolicyId) {
-        RedisSerializer<String> keySerializer = couponRedisTemplate.getStringSerializer();
-        RedisSerializer valueSerializer = couponRedisTemplate.getValueSerializer();
-
-        couponRedisTemplate.executePipelined((RedisCallback<Object>) redisConnection -> {
-            issueCoupons.forEach(issueCoupon ->
-                redisConnection.sAdd(
-                    Objects.requireNonNull(keySerializer.serialize(couponPolicyId)),
-                    valueSerializer.serialize(issueCoupon.getCode())));
-
-            return null;
-        });
+        issueCouponRepository.saveAll(issueCoupons);
     }
 
     private void createIssueCouponInLimitCount(CouponPolicy couponPolicy, Long issueQuantity) {
@@ -182,5 +171,45 @@ public class IssueCouponService {
     private void validBeforeProvide(Long accountId, CouponPolicy couponPolicy) {
         validPolicyDeleted(couponPolicy);
         validReceivedBefore(accountId, couponPolicy);
+    }
+
+    /**
+     * 이벤트를 통해 사용자에게 쿠폰을 발급하는 메서드.
+     *
+     * @param updateProvideCouponRequest the update provide coupon request
+     */
+    public void provideCouponToAccountByEvent(UpdateProvideCouponRequest updateProvideCouponRequest) {
+        CouponPolicy couponPolicy = couponPolicyRepository.findById(updateProvideCouponRequest.getCouponPolicyId())
+            .orElseThrow(CouponPolicyNotFoundException::new);
+
+        validBeforeProvide(updateProvideCouponRequest.getAccountId(), couponPolicy);
+
+        Account account = accountRepository.findById(updateProvideCouponRequest.getAccountId())
+            .orElseThrow(UserNotFoundException::new);
+
+        provideCouponUsingLock(couponPolicy, account);
+    }
+
+    private void provideCouponUsingLock(CouponPolicy couponPolicy, Account account) {
+        RLock lock = redissonClient.getLock(couponPolicy.getId().toString());
+
+        try {
+            boolean isLocked = lock.tryLock(WAIT_TIME, LEASE_TIME, TimeUnit.SECONDS);
+
+            if (!isLocked) {
+                throw new LockOverWaitTimeException();
+            }
+
+            IssueCoupon issueCoupon = issueCouponRepository.findByCouponPolicyAndAccountIsNull(couponPolicy)
+                .orElseThrow(CouponExhaustionException::new);
+            issueCoupon.provideToAccount(account);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new LockInterruptedException();
+
+        } finally {
+            lock.unlock();
+        }
     }
 }
