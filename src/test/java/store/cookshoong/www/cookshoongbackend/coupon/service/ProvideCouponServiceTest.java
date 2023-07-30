b90dd00b119b967a94081aed2727fd0d80e36982
eora21 <eora21@naver.com>
2023-07-30 22:08:13 +0900

    test: 💍 추가 쿠폰 이벤트 발급 서비스 테스트 작성

--- src/test/java/store/cookshoong/www/cookshoongbackend/coupon/service/ProvideCouponServiceTest.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/coupon/service/ProvideCouponServiceTest.java
@@ -3,6 +3,9 @@ package store.cookshoong.www.cookshoongbackend.coupon.service;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -10,6 +13,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +22,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.test.util.ReflectionTestUtils;
 import store.cookshoong.www.cookshoongbackend.account.entity.Account;
@@ -25,12 +30,14 @@ import store.cookshoong.www.cookshoongbackend.account.repository.AccountReposito
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.AlreadyHasCouponWithinSamePolicyException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponExhaustionException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponPolicyNotFoundException;
-import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponNotFoundException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.ProvideIssueCouponFailureException;
 import store.cookshoong.www.cookshoongbackend.coupon.model.request.UpdateProvideCouponRequestDto;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponPolicyRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.IssueCouponRepository;
+import store.cookshoong.www.cookshoongbackend.rabbitmq.exception.LockInterruptedException;
+import store.cookshoong.www.cookshoongbackend.rabbitmq.exception.LockOverWaitTimeException;
 import store.cookshoong.www.cookshoongbackend.util.TestEntity;
 import store.cookshoong.www.cookshoongbackend.util.TestPersistEntity;
 
@@ -63,7 +70,7 @@ class ProvideCouponServiceTest {
     @Test
     @DisplayName("쿠폰 발급 실패 - 쿠폰 정책 미존재")
     void provideCouponToAccountNonPolicyFailTest() throws Exception {
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.empty());
 
         assertThrowsExactly(CouponPolicyNotFoundException.class,
@@ -78,7 +85,7 @@ class ProvideCouponServiceTest {
 
         couponPolicy.delete();
 
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.of(couponPolicy));
 
         assertThrowsExactly(CouponPolicyNotFoundException.class,
@@ -91,7 +98,7 @@ class ProvideCouponServiceTest {
         CouponPolicy couponPolicy = te.persist(
             te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
 
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.of(couponPolicy));
 
         IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
@@ -100,7 +107,7 @@ class ProvideCouponServiceTest {
         when(issueCouponRepository.findTop10ByCouponPolicyAndAccountIsNull(any(CouponPolicy.class)))
             .thenReturn(Collections.emptyList());
 
-        assertThrowsExactly(IssueCouponNotFoundException.class,
+        assertThrowsExactly(CouponExhaustionException.class,
             () -> provideCouponService.provideCouponToAccountByApi(updateProvideCouponRequestDto));
     }
 
@@ -110,11 +117,11 @@ class ProvideCouponServiceTest {
         CouponPolicy couponPolicy = te.persist(
             te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
 
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.of(couponPolicy));
 
         Account customer = tpe.getLevelOneActiveCustomer();
-        when(accountRepository.getReferenceById(any(Long.class)))
+        when(accountRepository.getReferenceById(anyLong()))
             .thenReturn(customer);
 
         IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
@@ -133,13 +140,13 @@ class ProvideCouponServiceTest {
         CouponPolicy couponPolicy = te.persist(
             te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
 
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.of(couponPolicy));
 
         IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
         ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
 
-        when(issueCouponRepository.isReceivedBefore(any(Long.class), any(Long.class), any(Integer.class)))
+        when(issueCouponRepository.isReceivedBefore(anyLong(), anyLong(), any(Integer.class)))
             .thenReturn(true);
 
         assertThrowsExactly(AlreadyHasCouponWithinSamePolicyException.class,
@@ -153,7 +160,7 @@ class ProvideCouponServiceTest {
         CouponPolicy couponPolicy = te.persist(
             te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
 
-        when(couponPolicyRepository.findById(any(Long.class)))
+        when(couponPolicyRepository.findById(anyLong()))
             .thenReturn(Optional.of(couponPolicy));
 
         IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
@@ -163,12 +170,159 @@ class ProvideCouponServiceTest {
             .thenReturn(List.of(issueCoupon));
 
         Account customer = tpe.getLevelOneActiveCustomer();
-        when(accountRepository.getReferenceById(any(Long.class)))
+        when(accountRepository.getReferenceById(anyLong()))
             .thenReturn(customer);
 
         when(issueCouponRepository.provideCouponToAccount(any(UUID.class), any(LocalDate.class), any(Account.class)))
             .thenReturn(true);
 
         assertDoesNotThrow(() -> provideCouponService.provideCouponToAccountByApi(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - 쿠폰 정책 미존재")
+    void provideCouponToAccountByEventNonPolicyFailTest() throws Exception {
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.empty());
+
+        assertThrowsExactly(CouponPolicyNotFoundException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - 삭제된 쿠폰 정책")
+    void provideCouponToAccountByEventDeletedPolicyFailTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        couponPolicy.delete();
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        assertThrowsExactly(CouponPolicyNotFoundException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - 유효 쿠폰 없음")
+    void provideCouponToAccountByEventNonIssueCouponsFailTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
+        ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
+
+        RLock lock = mock(RLock.class);
+
+        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
+            .thenReturn(true);
+
+        when(redissonClient.getLock(anyString()))
+            .thenReturn(lock);
+
+        assertThrowsExactly(CouponExhaustionException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - 이미 발급한 기록이 있음")
+    void provideCouponToAccountByEventIssueAlreadyFailTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
+        ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
+
+        when(issueCouponRepository.isReceivedBefore(anyLong(), anyLong(), any(Integer.class)))
+            .thenReturn(true);
+
+        assertThrowsExactly(AlreadyHasCouponWithinSamePolicyException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - lock 시간 제한")
+    void provideCouponToAccountByEventLockTimeoutFailTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
+        ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
+
+        RLock lock = mock(RLock.class);
+
+        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
+            .thenReturn(false);
+
+        when(redissonClient.getLock(anyString()))
+            .thenReturn(lock);
+
+        assertThrowsExactly(LockOverWaitTimeException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 실패 - 인터럽트 발생")
+    void provideCouponToAccountByEventInterruptFailTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
+        ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
+
+        RLock lock = mock(RLock.class);
+
+        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
+            .thenThrow(InterruptedException.class);
+
+        when(redissonClient.getLock(anyString()))
+            .thenReturn(lock);
+
+        assertThrowsExactly(LockInterruptedException.class,
+            () -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
+    }
+
+
+    @Test
+    @DisplayName("쿠폰 이벤트 발급 성공")
+    void provideCouponToAccountByEventSuccessTest() throws Exception {
+        CouponPolicy couponPolicy = te.persist(
+            te.getCouponPolicy(te.getCouponTypeCash_1000_10000(), te.getCouponUsageStore(tpe.getOpenStore())));
+
+        when(couponPolicyRepository.findById(anyLong()))
+            .thenReturn(Optional.of(couponPolicy));
+
+        IssueCoupon issueCoupon = new IssueCoupon(couponPolicy);
+        ReflectionTestUtils.setField(issueCoupon, "code", UUID.randomUUID());
+
+        Account customer = tpe.getLevelOneActiveCustomer();
+        when(accountRepository.getReferenceById(anyLong()))
+            .thenReturn(customer);
+
+        RLock lock = mock(RLock.class);
+
+        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
+            .thenReturn(true);
+
+        when(redissonClient.getLock(anyString()))
+            .thenReturn(lock);
+
+        when(issueCouponRepository.findByCouponPolicyAndAccountIsNull(any(CouponPolicy.class)))
+            .thenReturn(Optional.of(issueCoupon));
+
+        assertDoesNotThrow(() -> provideCouponService.provideCouponToAccountByEvent(updateProvideCouponRequestDto));
     }
 }
