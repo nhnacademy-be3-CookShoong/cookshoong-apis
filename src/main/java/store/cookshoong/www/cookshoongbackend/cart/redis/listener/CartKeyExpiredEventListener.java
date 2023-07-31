dff00a35bc8bbf3488e127567380ffcbd8d7ef19
정제완 <jewan8821@naver.com>
2023-07-31 11:12:02 +0900

    feat: 🎸 추가 Phantom key 적용 에 필요한 listener 기능 구현

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/cart/redis/listener/CartKeyExpiredEventListener.java
@@ -0,0 +1,59 @@
+package store.cookshoong.www.cookshoongbackend.cart.redis.listener;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+import store.cookshoong.www.cookshoongbackend.cart.db.service.CartService;
+import store.cookshoong.www.cookshoongbackend.cart.redis.model.vo.CartRedisDto;
+import store.cookshoong.www.cookshoongbackend.cart.redis.service.CartRedisService;
+
+/**
+ * {설명을 작성해주세요}.
+ *
+ * @author jeongjewan
+ * @since 2023.07.27
+ */
+@Slf4j
+@Component
+public class CartKeyExpiredEventListener extends KeyExpirationEventMessageListener {
+
+    private static final String PHANTOM = ":phantom";
+    private final CartService cartService;
+    private final CartRedisService cartRedisService;
+
+    /**
+     * CartKeyExpiredEventListener 생성자.
+     *
+     * @param listenerContainer     만료된 key 에 대한 MessageListener
+     * @param cartService           DB 장바구니에 대한 Service
+     */
+    public CartKeyExpiredEventListener(RedisMessageListenerContainer listenerContainer,
+                                       CartService cartService, CartRedisService cartRedisService) {
+
+        super(listenerContainer);
+        this.cartService = cartService;
+        this.cartRedisService = cartRedisService;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+
+        String expiredRedisKey = message.toString();
+
+         log.error("EXPIRED KEY: {}", expiredRedisKey);
+
+        if (!expiredRedisKey.endsWith(PHANTOM)) {
+            return;
+        }
+
+        String redisKey = expiredRedisKey.replaceAll(PHANTOM, "");
+        log.error("REDIS KEY: {}", redisKey);
+
+        List<CartRedisDto> cartRedisList = cartRedisService.selectCartMenuAll(redisKey);
+
+        cartService.createCartDb(redisKey, cartRedisList);
+    }
+}
