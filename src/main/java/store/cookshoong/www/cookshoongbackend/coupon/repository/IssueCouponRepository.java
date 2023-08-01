7462e68bd17a66dd00b117f12df32aecce41140b
eora21 <eora21@naver.com>
2023-08-01 21:22:18 +0900

    refactor: 💡 삭제 사용하지 않는 발급 쿠폰 메서드 제거

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/repository/IssueCouponRepository.java
@@ -1,7 +1,6 @@
 package store.cookshoong.www.cookshoongbackend.coupon.repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
@@ -21,12 +20,4 @@ public interface IssueCouponRepository extends JpaRepository<IssueCoupon, UUID>,
      * @return the list
      */
     List<IssueCoupon> findTop10ByCouponPolicyAndAccountIsNull(CouponPolicy couponPolicy);
-
-    /**
-     * 사용자에게 발급되지 않은 쿠폰을 1개만 찾아오는 메서드.
-     *
-     * @param couponPolicy the coupon policy
-     * @return the optional
-     */
-    Optional<IssueCoupon> findByCouponPolicyAndAccountIsNull(CouponPolicy couponPolicy);
 }
