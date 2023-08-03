d016d169eefe5ba8d66f9e030d0c74b5491be544
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-04 01:26:50 +0900

    feat: 🎸 수정 OauthAccount 엔터티 복합키로 변경
    OAuth 회원의 엔터티가 회원시퀀스가 주키로 잡혀있다보니 한 회원이 여러 OAuth 가입이 막혀있어 OAuth 공급자 타입도 주키로 추가해주어 한 아이닥 여러 OAuth 공급자와 연동할 수 있게 하였습니다.
    * 로그인해서 연동은 가능하나 새로운 OAuth 로그인시 연동은 불가능합니다(OAuth 식별자로 기존 회원 식별이 불가능하기 때문)

--- src/main/java/store/cookshoong/www/cookshoongbackend/account/entity/OauthAccount.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/account/entity/OauthAccount.java
@@ -1,6 +1,9 @@
 package store.cookshoong.www.cookshoongbackend.account.entity;
 
+import java.io.Serializable;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,6 +15,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,20 +31,46 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "oauth_accounts")
 public class OauthAccount {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "account_id", nullable = false)
-    private Long id;
 
-    @MapsId
+    @EmbeddedId
+    private Pk pk;
+
+    @MapsId("id")
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
+    @MapsId("oauthTypeId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "oauth_type_id", nullable = false)
     private OauthType oauthType;
 
     @Column(name = "account_code", nullable = false)
     private String accountCode;
+
+    /**
+     * OAuthAccount 복합키.
+     */
+    @Embeddable
+    @EqualsAndHashCode
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class Pk implements Serializable {
+        private Long id;
+        private Integer oauthTypeId;
+    }
+
+    /**
+     * Instantiates a new Oauth account.
+     *
+     * @param account     the account
+     * @param oauthType   the oauth type
+     * @param accountCode the account code
+     */
+    public OauthAccount(Account account, OauthType oauthType, String accountCode) {
+        this.pk = new Pk(account.getId(), oauthType.getId());
+        this.account = account;
+        this.oauthType = oauthType;
+        this.accountCode = accountCode;
+    }
 }
