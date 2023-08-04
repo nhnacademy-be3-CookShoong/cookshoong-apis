e97c2f6b17e062e61a4e9b8b2f7f663fd91e90c4
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-04 12:41:08 +0900

    test: :ring: 테스트 매장관련 (은행, 가맹점, 카테고리) 서비스 테스트 코드작성

--- src/test/java/store/cookshoong/www/cookshoongbackend/shop/service/StoreCategoryServiceTest.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/shop/service/StoreCategoryServiceTest.java
@@ -1,14 +1,18 @@
 package store.cookshoong.www.cookshoongbackend.shop.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,8 +20,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
+import store.cookshoong.www.cookshoongbackend.shop.entity.StoreCategory;
+import store.cookshoong.www.cookshoongbackend.shop.exception.category.DuplicatedStoreCategoryException;
+import store.cookshoong.www.cookshoongbackend.shop.exception.category.StoreCategoryNotFoundException;
+import store.cookshoong.www.cookshoongbackend.shop.model.request.CreateStoreCategoryRequestDto;
+import store.cookshoong.www.cookshoongbackend.shop.model.request.UpdateStoreCategoryRequestDto;
 import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllCategoriesResponseDto;
-import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllMerchantsResponseDto;
 import store.cookshoong.www.cookshoongbackend.shop.repository.category.StoreCategoryRepository;
 
 /**
@@ -61,6 +70,89 @@ class StoreCategoryServiceTest {
     }
 
     @Test
+    @DisplayName("매장 카테고리 추가 - 성공")
+    void createStoreCategory() {
+        CreateStoreCategoryRequestDto storeCategory = ReflectionUtils.newInstance(CreateStoreCategoryRequestDto.class);
+        ReflectionTestUtils.setField(storeCategory, "storeCategoryCode", "CHK");
+        ReflectionTestUtils.setField(storeCategory, "storeCategoryName", "치킨");
+
+        when(storeCategoryRepository.existsById(storeCategory.getStoreCategoryCode())).thenReturn(false);
+        when(storeCategoryRepository.existsByDescription(storeCategory.getStoreCategoryName())).thenReturn(false);
+
+        storeCategoryService.createStoreCategory(storeCategory);
+
+        verify(storeCategoryRepository, times(1)).existsById(storeCategory.getStoreCategoryCode());
+        verify(storeCategoryRepository, times(1)).existsByDescription(storeCategory.getStoreCategoryName());
+        verify(storeCategoryRepository, times(1)).save(any(StoreCategory.class));
+    }
+
+    @Test
+    @DisplayName("카테고리 수정 - 성공")
+    void updateStoreCategory() {
+        String categoryCode = "CHK";
+        UpdateStoreCategoryRequestDto updateCategoryDto = ReflectionUtils.newInstance(UpdateStoreCategoryRequestDto.class);
+        ReflectionTestUtils.setField(updateCategoryDto, "storeCategoryName", "치킨");
+
+        StoreCategory storeCategory = new StoreCategory("CHK", "NONE");
+
+        when(storeCategoryRepository.findById(categoryCode)).thenReturn(Optional.of(storeCategory));
+        when(storeCategoryRepository.existsByDescription(updateCategoryDto.getStoreCategoryName())).thenReturn(false);
+
+        storeCategoryService.updateStoreCategory(categoryCode, updateCategoryDto);
+
+        verify(storeCategoryRepository, times(1)).findById(categoryCode);
+        verify(storeCategoryRepository, times(1)).existsByDescription(updateCategoryDto.getStoreCategoryName());
+
+        assertThat(storeCategory.getDescription()).isEqualTo(updateCategoryDto.getStoreCategoryName());
+    }
+
+    @Test
+    @DisplayName("카테고리 수정 - 실패 : 이미 존재하는 카테고리명")
+    void updateStoreCategory_fail_duplicated() {
+        String categoryCode = "CHK";
+        UpdateStoreCategoryRequestDto updateCategoryDto = ReflectionUtils.newInstance(UpdateStoreCategoryRequestDto.class);
+        ReflectionTestUtils.setField(updateCategoryDto, "storeCategoryName", "치킨");
+
+        StoreCategory storeCategory = new StoreCategory("CHK", "NONE");
+
+        when(storeCategoryRepository.findById(categoryCode)).thenReturn(Optional.of(storeCategory));
+        when(storeCategoryRepository.existsByDescription(updateCategoryDto.getStoreCategoryName())).thenReturn(true);
+
+        assertThatThrownBy(
+            () -> storeCategoryService.updateStoreCategory(categoryCode, updateCategoryDto))
+            .isInstanceOf(DuplicatedStoreCategoryException.class)
+                .hasMessageContaining("치킨은 이미 등록되어 있습니다.");
+
+        verify(storeCategoryRepository, times(1)).findById(categoryCode);
+        verify(storeCategoryRepository, times(1)).existsByDescription(updateCategoryDto.getStoreCategoryName());
+
+        assertThat(storeCategory.getDescription()).isNotEqualTo(updateCategoryDto.getStoreCategoryName());
+    }
+
+    @Test
+    @DisplayName("카테고리 수정 - 실패 : 존재하지 않는 카테고리")
+    void updateStoreCategory_fail_notFound() {
+        String categoryCode = "NONE";
+        UpdateStoreCategoryRequestDto updateCategoryDto = ReflectionUtils.newInstance(UpdateStoreCategoryRequestDto.class);
+        ReflectionTestUtils.setField(updateCategoryDto, "storeCategoryName", "치킨");
+
+        StoreCategory storeCategory = new StoreCategory("CHK", "NONE");
+
+        when(storeCategoryRepository.findById(categoryCode)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(
+            () -> storeCategoryService.updateStoreCategory(categoryCode, updateCategoryDto))
+            .isInstanceOf(StoreCategoryNotFoundException.class)
+            .hasMessageContaining("해당 카테고리는 존재하지 않습니다.");
+
+        verify(storeCategoryRepository, times(1)).findById(categoryCode);
+        verify(storeCategoryRepository, times(0)).existsByDescription(updateCategoryDto.getStoreCategoryName());
+
+        assertThat(storeCategory.getDescription()).isNotEqualTo(updateCategoryDto.getStoreCategoryName());
+    }
+
+    @Test
+    @DisplayName("카테고리 리스트 조회 - 성공")
     void selectAllCategoriesForUser() {
         // Given
         List<SelectAllCategoriesResponseDto> selectAllCategoriesResponseDtos = List.of(
