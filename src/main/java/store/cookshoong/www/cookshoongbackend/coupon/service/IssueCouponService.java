8ccd3c1944b425366e637b90410d745b98f57d80
eora21 <eora21@naver.com>
2023-08-04 11:10:54 +0900

    feat: 🎸 추가 쿠폰 코드로 발행된 쿠폰을 찾는 메서드 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/IssueCouponService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/coupon/service/IssueCouponService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponPolicy;
 import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponPolicyNotFoundException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponNotFoundException;
 import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponOverCountException;
 import store.cookshoong.www.cookshoongbackend.coupon.model.request.CreateIssueCouponRequestDto;
 import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponPolicyRepository;
@@ -103,5 +104,16 @@ public class IssueCouponService {
         if (LIMIT_COUNT < unclaimedCouponCount + issueQuantity) {
             throw new IssueCouponOverCountException(LIMIT_COUNT);
         }
+    }
+
+    /**
+     * 쿠폰 코드로 발행된 쿠폰을 찾는 메서드.
+     *
+     * @param issueCouponCode the issue coupon code
+     * @return the issue coupon
+     */
+    public IssueCoupon selectIssueCouponByCode(UUID issueCouponCode) {
+        return issueCouponRepository.findById(issueCouponCode)
+            .orElseThrow(IssueCouponNotFoundException::new);
     }
 }
