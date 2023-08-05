523fdd34bfb22b0b6e8e84aea5b8282c611fc065
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-05 23:39:21 +0900

    refactor: :bulb: 수정 cateogories 경로 변경

--- src/main/java/store/cookshoong/www/cookshoongbackend/shop/controller/StoreController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/shop/controller/StoreController.java
@@ -1,5 +1,6 @@
 package store.cookshoong.www.cookshoongbackend.shop.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import store.cookshoong.www.cookshoongbackend.coupon.service.CouponPolicyService;
+import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectAllCategoriesResponseDto;
 import store.cookshoong.www.cookshoongbackend.shop.model.response.SelectStoreForUserResponseDto;
+import store.cookshoong.www.cookshoongbackend.shop.service.StoreCategoryService;
 import store.cookshoong.www.cookshoongbackend.shop.service.StoreService;
 
 /**
@@ -23,6 +26,7 @@ import store.cookshoong.www.cookshoongbackend.shop.service.StoreService;
 public class StoreController {
     private final StoreService storeService;
     private final CouponPolicyService couponPolicyService;
+    private final StoreCategoryService storeCategoryService;
 
     /**
      * 일반 유저 : 매장 정보 조회 페이지.
@@ -38,5 +42,16 @@ public class StoreController {
             couponPolicyService.getProvableStoreCouponPolicies(storeId));
 
         return ResponseEntity.ok(selectStoreForUserResponseDto);
+    }
+
+    /**
+     * 일반 유저 : 모든 카테고리 조회.
+     *
+     * @return the store categories
+     */
+    @GetMapping("/categories")
+    public ResponseEntity<List<SelectAllCategoriesResponseDto>> getStoreCategories() {
+        return ResponseEntity
+            .ok(storeCategoryService.selectAllCategoriesForUser());
     }
 }
