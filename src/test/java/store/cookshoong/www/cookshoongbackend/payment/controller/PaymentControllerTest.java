a3268dcef9b5357e4c12e457ee5a57e8e99c03ef
정제완 <jewan8821@naver.com>
2023-08-06 19:23:01 +0900

    test: 💍 수정 환불 및 결제 기능 추가로 인해 테스트 코드 추가 및 변경

--- src/test/java/store/cookshoong/www/cookshoongbackend/payment/controller/PaymentControllerTest.java
+++ src/test/java/store/cookshoong/www/cookshoongbackend/payment/controller/PaymentControllerTest.java
@@ -6,9 +6,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
@@ -21,9 +23,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import store.cookshoong.www.cookshoongbackend.payment.model.request.CreatePaymentDto;
+import store.cookshoong.www.cookshoongbackend.payment.model.request.CreateRefundDto;
+import store.cookshoong.www.cookshoongbackend.payment.model.response.TossPaymentKeyResponseDto;
 import store.cookshoong.www.cookshoongbackend.payment.service.PaymentService;
 
 /**
@@ -79,8 +86,7 @@ class PaymentControllerTest {
 
     @Test
     @DisplayName("결제 승인 후 결제가 완료되고나서 결제 정보를 DB 에 저장")
-    void postCreatePayment_validation_check() throws Exception {
-
+    void postCreatePayment_Validation_Exception() throws Exception {
 
         CreatePaymentDto createPaymentDto = ReflectionUtils.newInstance(CreatePaymentDto.class);
         ReflectionTestUtils.setField(createPaymentDto, "orderId", null);
@@ -93,6 +99,85 @@ class PaymentControllerTest {
                 .contentType(APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(createPaymentDto)))
             .andExpect(status().isBadRequest());
+    }
 
+    @Test
+    @DisplayName("주문에 대해서 환불되는 정보를 DB 에 저장")
+    void postCreateRefund() throws Exception {
+
+
+        CreateRefundDto createRefundDto = ReflectionUtils.newInstance(CreateRefundDto.class);
+        ReflectionTestUtils.setField(createRefundDto, "chargeCode", uuid);
+        ReflectionTestUtils.setField(createRefundDto, "refundAt", "2023-08-03T14:30:00+00:00");
+        ReflectionTestUtils.setField(createRefundDto, "refundAmount", 20000);
+        ReflectionTestUtils.setField(createRefundDto, "refundType", "partial");
+
+        mockMvc.perform(post("/api/payments/refunds")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createRefundDto)))
+            .andExpect(status().isCreated())
+            .andDo(document("post-create-refund",
+                ResourceSnippetParameters.builder()
+                    .requestSchema(schema("PostCreateRefund")),
+                requestFields(
+                    fieldWithPath("chargeCode").description("결제코드"),
+                    fieldWithPath("refundAt").description("환불시간"),
+                    fieldWithPath("refundAmount").description("환불금액"),
+                    fieldWithPath("refundType").description("환불타입")
+                )));
+
+        verify(paymentService, times(1)).createRefund(any(CreateRefundDto.class));
+    }
+
+    @Test
+    @DisplayName("주문에 대해서 환불되는 정보를 DB 에 저장")
+    void postCreateRefund_Validation_Exception() throws Exception {
+
+        CreateRefundDto createRefundDto = ReflectionUtils.newInstance(CreateRefundDto.class);
+        ReflectionTestUtils.setField(createRefundDto, "chargeCode", null);
+        ReflectionTestUtils.setField(createRefundDto, "refundAt", "2023-08-03T14:30:00+00:00");
+        ReflectionTestUtils.setField(createRefundDto, "refundAmount", 20000);
+        ReflectionTestUtils.setField(createRefundDto, "refundType", "partial");
+
+        mockMvc.perform(post("/api/payments/refunds")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createRefundDto)))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("토스 API 결제 조회 및 취소에 필요한 paymentKey 를 조회")
+    void getTossPaymentKey() throws Exception {
+        TossPaymentKeyResponseDto tossPaymentKeyResponseDto = new TossPaymentKeyResponseDto("paymentKey");
+
+        mockMvc.perform(get("/api/payments/{orderCode}/select-paymentKey", uuid)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tossPaymentKeyResponseDto)))
+            .andExpect(status().isOk())
+            .andDo(document("get-toss-payment-key",
+                ResourceSnippetParameters.builder()
+                    .pathParameters(parameterWithName("orderCode").description("주문코드"))
+                    .requestSchema(schema("GetTossPaymentKey")),
+                requestFields(
+                    fieldWithPath("paymentKey").description("결제키")
+                )));
+    }
+
+    @Test
+    @DisplayName("해당 결제에 대해 환불금액이 결제금액보다 넘어가는지 검증")
+    void getIsRefundAmountExceedsChargedAmount() throws Exception {
+
+        RequestBuilder request = RestDocumentationRequestBuilders
+            .get("/api/payments/charges/{chargeCode}/refunds/verify", uuid)
+            .param("refundAmount", "20000")
+            .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+            .andExpect(status().isOk())
+            .andDo(document("get-is-refundAmount-exceeds-chargedAmount",
+                ResourceSnippetParameters.builder()
+                    .pathParameters(parameterWithName("chargeCode").description("결제코드"))
+                    .pathParameters(parameterWithName("refundAmount").description("환불금액"))
+                    .requestSchema(schema("GetIsRefundAmountExceedsChargedAmount"))));
     }
 }
