0a1d566927c140ce22fed0da31f50af29738603c
윤동현 <eatpaper1429@gmail.com>
2023-08-07 16:08:19 +0900

    refactor: 💡 수정 메인 페이지 Outed 함수를 Elasticsearch 함수로 변경

--- src/main/java/store/cookshoong/www/cookshoongbackend/search/controller/StoreDocumentController.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/search/controller/StoreDocumentController.java
@@ -5,14 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import store.cookshoong.www.cookshoongbackend.file.model.FileDomain;
 import store.cookshoong.www.cookshoongbackend.file.service.ObjectStorageService;
-import store.cookshoong.www.cookshoongbackend.search.model.StoreDocumentRequestAllDto;
 import store.cookshoong.www.cookshoongbackend.search.model.StoreDocumentResponseDto;
 import store.cookshoong.www.cookshoongbackend.search.service.StoreDocumentService;
 import store.cookshoong.www.cookshoongbackend.shop.service.StoreCategoryService;
@@ -30,11 +27,6 @@ public class StoreDocumentController {
     private final StoreDocumentService storeDocumentService;
     private final StoreCategoryService storeCategoryService;
     private final ObjectStorageService objectStorageService;
-
-    @PutMapping("/store/keyword")
-    public void saveByKeyword(@RequestBody StoreDocumentRequestAllDto storeDocumentRequestAllDto) {
-        storeDocumentService.saveAll(storeDocumentRequestAllDto);
-    }
 
     @GetMapping("/store/list")
     public ResponseEntity<Page<StoreDocumentResponseDto>> searchByDistance(
--- src/main/java/store/cookshoong/www/cookshoongbackend/search/service/StoreDocumentService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/search/service/StoreDocumentService.java
@@ -1,14 +1,11 @@
 package store.cookshoong.www.cookshoongbackend.search.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import store.cookshoong.www.cookshoongbackend.search.model.StoreDocument;
-import store.cookshoong.www.cookshoongbackend.search.model.StoreDocumentRequestAllDto;
 import store.cookshoong.www.cookshoongbackend.search.model.StoreDocumentResponseDto;
 import store.cookshoong.www.cookshoongbackend.search.repository.StoreSearchRepository;
 import store.cookshoong.www.cookshoongbackend.shop.repository.category.StoreCategoryRepository;
@@ -25,13 +22,6 @@ import store.cookshoong.www.cookshoongbackend.shop.repository.category.StoreCate
 public class StoreDocumentService {
     private final StoreSearchRepository storeSearchRepository;
     private final StoreCategoryRepository storesCategoryRepository;
-
-    @Transactional
-    public void saveAll(StoreDocumentRequestAllDto storeDocumentRequestAllDto) {
-        List<StoreDocument> storeList =
-            storeDocumentRequestAllDto.getStoreDocumentRequestDtoList().stream().map(StoreDocument::from).collect(Collectors.toList());
-        storeSearchRepository.saveAll(storeList);
-    }
 
     public Page<StoreDocumentResponseDto> searchByKeywordText(String keyword, Long addressId, Pageable pageable) {
         Page<StoreDocument> storeDocuments = storeSearchRepository.searchByKeywordText(keyword, addressId, pageable);
