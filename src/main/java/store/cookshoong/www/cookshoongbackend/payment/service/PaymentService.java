536805ecf52e577963a9554bb774b044d397fef4
eora21 <eora21@naver.com>
2023-08-07 13:35:25 +0900

    feat: 🎸 추가 쿠폰 사용하는 메서드 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/payment/service/PaymentService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/payment/service/PaymentService.java
@@ -2,11 +2,22 @@ package store.cookshoong.www.cookshoongbackend.payment.service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponLog;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.CouponLogType;
+import store.cookshoong.www.cookshoongbackend.coupon.entity.IssueCoupon;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.AlreadyUsedCouponException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.CouponLogTypeNotFoundException;
+import store.cookshoong.www.cookshoongbackend.coupon.exception.IssueCouponNotFoundException;
+import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponLogRepository;
+import store.cookshoong.www.cookshoongbackend.coupon.repository.CouponLogTypeRepository;
+import store.cookshoong.www.cookshoongbackend.coupon.repository.IssueCouponRepository;
 import store.cookshoong.www.cookshoongbackend.order.entity.Order;
 import store.cookshoong.www.cookshoongbackend.order.exception.OrderNotFoundException;
 import store.cookshoong.www.cookshoongbackend.order.repository.OrderRepository;
@@ -23,6 +34,7 @@ import store.cookshoong.www.cookshoongbackend.payment.repository.charge.ChargeRe
 import store.cookshoong.www.cookshoongbackend.payment.repository.chargetype.ChargeTypeRepository;
 import store.cookshoong.www.cookshoongbackend.payment.repository.refund.RefundRepository;
 import store.cookshoong.www.cookshoongbackend.payment.repository.refundtype.RefundTypeRepository;
+import store.cookshoong.www.cookshoongbackend.util.Locker;
 
 /**
  * 결제에 대한 Service.
@@ -41,16 +53,25 @@ public class PaymentService {
     private final RefundRepository refundRepository;
     private final RefundTypeRepository refundTypeRepository;
     private final OrderRepository orderRepository;
+    private final IssueCouponRepository issueCouponRepository;
+    private final CouponLogRepository couponLogRepository;
+    private final CouponLogTypeRepository couponLogTypeRepository;
+    private final Locker locker;
 
     /**
      * 결제 승인 후 결제가 완료되고나서 결제 정보를 DB 에 저장하는 메서드.
      *
-     * @param createPaymentDto      결제 성공한 정보 Dto
+     * @param createPaymentDto 결제 성공한 정보 Dto
      */
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public void createPayment(CreatePaymentDto createPaymentDto) {
-
         Order order =
             orderRepository.findById(createPaymentDto.getOrderId()).orElseThrow(OrderNotFoundException::new);
+
+        if (Objects.nonNull(createPaymentDto.getCouponCode())) {
+            useCoupon(createPaymentDto, order);
+        }
+
         ChargeType chargeType =
             chargeTypeRepository.findById(
                 createPaymentDto.getPaymentType()).orElseThrow(ChargeTypeNotFoundException::new);
@@ -62,10 +83,31 @@ public class PaymentService {
         chargeRepository.save(charge);
     }
 
+    private void useCoupon(CreatePaymentDto createPaymentDto, Order order) {
+        IssueCoupon issueCoupon = issueCouponRepository.findById(createPaymentDto.getCouponCode())
+            .orElseThrow(IssueCouponNotFoundException::new);
+
+        locker.lock(issueCoupon.getCode().toString(), ignore -> {
+            couponLogRepository.findTopByIssueCouponOrderByIdDesc(issueCoupon)
+                .ifPresent(this::validCouponLog);
+
+            CouponLogType couponLogType = couponLogTypeRepository.findById(CouponLogType.Code.USE.toString())
+                .orElseThrow(CouponLogTypeNotFoundException::new);
+            couponLogRepository.saveAndFlush(new CouponLog(issueCoupon, couponLogType, order));
+        });
+    }
+
+    private void validCouponLog(CouponLog couponLog) {
+        if (couponLog.getCouponLogType().getCode().equals(CouponLogType.Code.USE.toString())) {
+            throw new AlreadyUsedCouponException();
+        }
+    }
+
+
     /**
      * 주문에 대해서 취소할 때 방생하는 환불 데이터를 저장하는 메서드.
      *
-     * @param createRefundDto       환불에 대한 Dto
+     * @param createRefundDto 환불에 대한 Dto
      */
     public void createRefund(CreateRefundDto createRefundDto) {
 
@@ -86,8 +128,8 @@ public class PaymentService {
     /**
      * orderCode 를 통해서 결제에 저장되어 있는 paymentKey 를 가져오는 메서드.
      *
-     * @param orderCode     주문 코드
-     * @return              paymentKey 반환
+     * @param orderCode 주문 코드
+     * @return paymentKey 반환
      */
     @Transactional(readOnly = true)
     public TossPaymentKeyResponseDto selectTossPaymentKey(UUID orderCode) {
@@ -98,9 +140,9 @@ public class PaymentService {
     /**
      * 환불 금액이 결제 금액 보다 넘어가는지 검증하는 메서드.
      *
-     * @param refundAmount      현재 환불되는 금액
-     * @param chargeCode        이전 결제된 금액
-     * @return                  환불 금액이 결제 금액을 넘으면 true, 환불 금액이 결제 금애보다 작으면 false
+     * @param refundAmount 현재 환불되는 금액
+     * @param chargeCode   이전 결제된 금액
+     * @return 환불 금액이 결제 금액을 넘으면 true, 환불 금액이 결제 금애보다 작으면 false
      */
     public boolean isRefundAmountExceedsChargedAmount(Integer refundAmount, UUID chargeCode) {
 
@@ -111,7 +153,7 @@ public class PaymentService {
     }
 
     /**
-     *  String 형태를 LocalDateTime 으로 변환기.
+     * String 형태를 LocalDateTime 으로 변환기.
      */
     private LocalDateTime getApproveChargeAt(String chargeAt) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
