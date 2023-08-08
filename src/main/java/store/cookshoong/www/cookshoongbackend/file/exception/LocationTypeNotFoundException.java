bab1c71bfd6f3a9ae947e18a641c70cdf5c0f944
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-08 10:47:19 +0900

    refactor: :bulb: 수정 매장 등록Dto에서 Integer 타입은 ```@NotNull```로 변경

--- src/main/java/store/cookshoong/www/cookshoongbackend/file/exception/LocationTypeNotFoundException.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/file/exception/LocationTypeNotFoundException.java
@@ -3,7 +3,7 @@ package store.cookshoong.www.cookshoongbackend.file.exception;
 import store.cookshoong.www.cookshoongbackend.common.exception.NotFoundException;
 
 /**
- * 파일 저장소타입에 대한 예외.
+ * 없는 저장소 타입 요청에 대한 예외.
  *
  * @author seungyeon
  * @since 2023.08.08
