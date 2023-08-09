b126e42b6ae34c7ffa4836e5aa7dd5b4248a6f06
eora21 <eora21@naver.com>
2023-08-09 15:21:07 +0900

    feat: 🎸 추가 내 포인트 확인, 포인트 로그 확인, 특정 주문에 대한 포인트 확인 메서드 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/point/repository/PointLogRepositoryImpl.java
@@ -0,0 +1,98 @@
+package store.cookshoong.www.cookshoongbackend.point.repository;
+
+import static store.cookshoong.www.cookshoongbackend.point.entity.QPointLog.pointLog;
+import static store.cookshoong.www.cookshoongbackend.point.entity.QPointReason.pointReason;
+import static store.cookshoong.www.cookshoongbackend.point.entity.QPointReasonOrder.pointReasonOrder;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import store.cookshoong.www.cookshoongbackend.account.entity.Account;
+import store.cookshoong.www.cookshoongbackend.order.entity.Order;
+import store.cookshoong.www.cookshoongbackend.point.entity.PointLog;
+import store.cookshoong.www.cookshoongbackend.point.model.response.PointLogResponseDto;
+import store.cookshoong.www.cookshoongbackend.point.model.response.PointResponseDto;
+import store.cookshoong.www.cookshoongbackend.point.model.response.QPointLogResponseDto;
+import store.cookshoong.www.cookshoongbackend.point.model.response.QPointResponseDto;
+
+/**
+ * 포인트 로그 QueryDSL.
+ *
+ * @author eora21 (김주호)
+ * @since 2023.08.08
+ */
+@RequiredArgsConstructor
+public class PointLogRepositoryImpl implements PointLogRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PointResponseDto lookupMyPoint(Account account) {
+        return queryFactory
+            .select(new QPointResponseDto(pointLog.pointMovement.sum()))
+            .from(pointLog)
+            .where(pointLog.account.eq(account))
+            .fetchOne();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Page<PointLogResponseDto> lookupMyPointLog(Account account, Pageable pageable) {
+        List<PointLogResponseDto> couponLog = getCouponLog(account, pageable);
+        Long couponLogTotalSize = getCouponLogTotalSize(account);
+        return new PageImpl<>(couponLog, pageable, couponLogTotalSize);
+    }
+
+    private List<PointLogResponseDto> getCouponLog(Account account, Pageable pageable) {
+        return queryFactory
+            .select(new QPointLogResponseDto(pointReason, pointLog.pointMovement, pointLog.pointAt))
+            .from(pointLog)
+
+            .innerJoin(pointLog.pointReason, pointReason)
+
+            .where(pointLog.account.eq(account))
+            .orderBy(pointLog.id.desc())
+
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+
+            .fetch();
+    }
+
+    private Long getCouponLogTotalSize(Account account) {
+        return queryFactory
+            .select(pointLog.count())
+            .from(pointLog)
+
+            .innerJoin(pointLog.pointReason, pointReason)
+
+            .where(pointLog.account.eq(account))
+
+            .fetchOne();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<PointLog> lookupPointCompleteAmount(Order order) {
+        return queryFactory
+            .select(pointLog)
+            .from(pointReasonOrder)
+
+            .innerJoin(pointReasonOrder)
+            .on(pointReasonOrder.order.eq(order))
+
+            .innerJoin(pointLog)
+            .on(pointLog.pointReason.id.eq(pointReasonOrder.id))
+
+            .fetch();
+    }
+}
