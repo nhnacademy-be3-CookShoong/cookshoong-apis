ef491e61ae91de84ffda18c8feac7ebaffc322ac
eora21 <eora21@naver.com>
2023-08-09 14:49:00 +0900

    feat: 🎸 추가 하나의 주문에 대한 포인트 적립이 2개 이상일 시 발생하는 예외 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/point/exception/OrderPointLogDuplicateException.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongbackend.point.exception;
+
+/**
+ * 하나의 주문에 대한 포인트 적립이 2개 이상일 시 발생하는 예외.
+ *
+ * @author eora21 (김주호)
+ * @since 2023.08.09
+ */
+public class OrderPointLogDuplicateException extends RuntimeException {
+    public OrderPointLogDuplicateException() {
+        super("해당 주문으로 획득한 포인트가 정상 검색되지 않았습니다. 관리자에게 문의해주세요.");
+    }
+}
