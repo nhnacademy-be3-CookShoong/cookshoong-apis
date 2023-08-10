8f7664e6adc85f840d642590666639b65f2098d0
eora21 <eora21@naver.com>
2023-08-10 21:09:07 +0900

    feat: 🎸 추가 진행중인 주문을 확인하는 repositoryImpl 메서드 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/order/repository/OrderRepositoryImpl.java
@@ -0,0 +1,99 @@
+package store.cookshoong.www.cookshoongbackend.order.repository;
+
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
+import static store.cookshoong.www.cookshoongbackend.menu_order.entity.menu.QMenu.menu;
+import static store.cookshoong.www.cookshoongbackend.order.entity.QOrder.order;
+import static store.cookshoong.www.cookshoongbackend.order.entity.QOrderDetail.orderDetail;
+import static store.cookshoong.www.cookshoongbackend.order.entity.QOrderDetailMenuOption.orderDetailMenuOption;
+import static store.cookshoong.www.cookshoongbackend.order.entity.QOrderStatus.orderStatus;
+import static store.cookshoong.www.cookshoongbackend.payment.entity.QCharge.charge;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import store.cookshoong.www.cookshoongbackend.order.model.response.LookupOrderDetailDto;
+import store.cookshoong.www.cookshoongbackend.order.model.response.LookupOrderDetailMenuOptionDto;
+import store.cookshoong.www.cookshoongbackend.order.model.response.LookupOrderInProgressDto;
+import store.cookshoong.www.cookshoongbackend.order.model.response.QLookupOrderDetailDto;
+import store.cookshoong.www.cookshoongbackend.order.model.response.QLookupOrderDetailMenuOptionDto;
+import store.cookshoong.www.cookshoongbackend.order.model.response.QLookupOrderInProgressDto;
+import store.cookshoong.www.cookshoongbackend.shop.entity.Store;
+
+/**
+ * 주문 관련 복잡한 쿼리를 처리하는 impl.
+ *
+ * @author eora21 (김주호)
+ * @since 2023.08.09
+ */
+@RequiredArgsConstructor
+public class OrderRepositoryImpl implements OrderRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<LookupOrderInProgressDto> lookupOrderInStatus(Store store, Set<String> orderStatusCode) {
+        List<LookupOrderInProgressDto> selectOrderInProgresses = getOrderInProgresses(store, orderStatusCode);
+
+        Set<Long> orderDetailIds = selectOrderInProgresses.stream()
+            .flatMap(selectOrderInProgressDto -> selectOrderInProgressDto.getSelectOrderDetails().stream()
+                .map(LookupOrderDetailDto::getOrderDetailId))
+            .collect(Collectors.toSet());
+
+        Map<Long, List<LookupOrderDetailMenuOptionDto>> selectOrderDetailMenuOptions =
+            getOrderDetailMenuOptions(orderDetailIds);
+
+        selectOrderInProgresses.stream()
+            .flatMap(selectOrderInProgress -> selectOrderInProgress.getSelectOrderDetails().stream())
+            .forEach(selectOrderDetail -> selectOrderDetail.setSelectOrderDetailMenuOptions(
+                selectOrderDetailMenuOptions.getOrDefault(selectOrderDetail.getOrderDetailId(), Collections.emptyList())
+            ));
+
+        return selectOrderInProgresses;
+    }
+
+    private List<LookupOrderInProgressDto> getOrderInProgresses(Store store, Set<String> orderStatusCode) {
+        return jpaQueryFactory
+            .selectFrom(order)
+            .innerJoin(order.orderStatus, orderStatus)
+
+            .innerJoin(orderDetail)
+            .on(orderDetail.order.eq(order))
+
+            .innerJoin(orderDetail.menu, menu)
+
+            .innerJoin(charge)
+            .on(charge.order.eq(order))
+
+            .where(order.store.eq(store), orderStatus.code.in(orderStatusCode))
+            .orderBy(order.orderedAt.asc())
+
+            .transform(
+                groupBy(order)
+                    .list(new QLookupOrderInProgressDto(
+                        order.code,
+                        orderStatus.code,
+                        list(new QLookupOrderDetailDto(
+                            orderDetail.id, orderDetail.nowName, menu.cookingTime, orderDetail.count)),
+                        order.memo,
+                        charge.code))
+            );
+    }
+
+    private Map<Long, List<LookupOrderDetailMenuOptionDto>> getOrderDetailMenuOptions(Set<Long> orderDetailIds) {
+        return jpaQueryFactory
+            .selectFrom(orderDetailMenuOption)
+            .innerJoin(orderDetailMenuOption.orderDetail, orderDetail)
+            .where(orderDetail.id.in(orderDetailIds))
+            .transform(
+                groupBy(orderDetail.id)
+                    .as(list(new QLookupOrderDetailMenuOptionDto(orderDetailMenuOption.nowName)))
+            );
+    }
+}
