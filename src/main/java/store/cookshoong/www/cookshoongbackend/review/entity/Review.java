132ab0a33cc26ca9d011ff052e5a5408cd852814
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-13 14:20:27 +0900

    feat: :guitar: 추가 review 작성에 필요한 엔티티 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/review/entity/Review.java
@@ -0,0 +1,72 @@
+package store.cookshoong.www.cookshoongbackend.review.entity;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import store.cookshoong.www.cookshoongbackend.order.entity.Order;
+import store.cookshoong.www.cookshoongbackend.review.model.request.CreateReviewRequestDto;
+
+/**
+ * 리뷰 엔티티.
+ *
+ * @author seungyeon (유승연)
+ * @since 2023.08.11
+ */
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_code", nullable = false)
+    private Order orderCode;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.PERSIST)
+    private final Set<ReviewHasImage> reviewHasImages = new HashSet<>();
+
+    @Lob
+    @Column(name = "contents", nullable = false)
+    private String contents;
+
+
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "written_at", nullable = false)
+    private LocalDateTime writtenAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    /**
+     * 리뷰 등록시 사용되는 생성자.
+     *
+     * @param orderCode  the order code
+     * @param requestDto the request dto
+     */
+    public Review(Order orderCode, CreateReviewRequestDto requestDto) {
+        this.orderCode = orderCode;
+        this.contents = requestDto.getContents();
+        this.rating = requestDto.getRating();
+        this.writtenAt = LocalDateTime.now();
+    }
+
+}
