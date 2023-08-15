53c6dc8a21ccea335f29dfa14c5ed4a23a03a1e7
eora21 <eora21@naver.com>
2023-08-15 22:20:55 +0900

    feat: 🎸 추가 내부 값이 null인 경우 스스로 판단하여 추후 제거하기 위한 인터페이스 작성

---
+++ src/main/java/store/cookshoong/www/cookshoongbackend/review/model/response/Remove.java
@@ -0,0 +1,11 @@
+package store.cookshoong.www.cookshoongbackend.review.model.response;
+
+/**
+ * 내부 값이 null인 경우 스스로 판단하여 추후 제거하기 위한 인터페이스.
+ *
+ * @author eora21 (김주호)
+ * @since 2023.08.15
+ */
+public interface Remove {
+    boolean isNull();
+}
