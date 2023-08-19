9cd2bb027dea0d7c0b97b6e055e474e2d7ac6e42
eora21 <eora21@naver.com>
2023-08-19 15:04:00 +0900

    feat: 🎸 추가 리뷰 포인트 제공 로직 작성

--- src/main/java/store/cookshoong/www/cookshoongbackend/point/service/PointEventService.java
+++ src/main/java/store/cookshoong/www/cookshoongbackend/point/service/PointEventService.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import store.cookshoong.www.cookshoongbackend.account.entity.Account;
-import store.cookshoong.www.cookshoongbackend.account.exception.UserNotFoundException;
 import store.cookshoong.www.cookshoongbackend.account.repository.AccountRepository;
 import store.cookshoong.www.cookshoongbackend.order.entity.Order;
 import store.cookshoong.www.cookshoongbackend.order.exception.OrderNotFoundException;
@@ -22,11 +21,14 @@ import store.cookshoong.www.cookshoongbackend.payment.exception.ChargeNotFoundEx
 import store.cookshoong.www.cookshoongbackend.payment.repository.charge.ChargeRepository;
 import store.cookshoong.www.cookshoongbackend.point.entity.PointLog;
 import store.cookshoong.www.cookshoongbackend.point.entity.PointReasonOrder;
+import store.cookshoong.www.cookshoongbackend.point.entity.PointReasonReview;
 import store.cookshoong.www.cookshoongbackend.point.entity.PointReasonSignup;
 import store.cookshoong.www.cookshoongbackend.point.exception.OrderPointLogDuplicateException;
 import store.cookshoong.www.cookshoongbackend.point.repository.PointLogRepository;
-import store.cookshoong.www.cookshoongbackend.point.repository.PointReasonOrderRepository;
 import store.cookshoong.www.cookshoongbackend.point.repository.PointReasonRepository;
+import store.cookshoong.www.cookshoongbackend.review.entity.Review;
+import store.cookshoong.www.cookshoongbackend.review.exception.ReviewNotFoundException;
+import store.cookshoong.www.cookshoongbackend.review.repository.ReviewRepository;
 
 /**
  * 포인트 이벤트 로직을 담당하는 서비스.
@@ -40,6 +42,10 @@ import store.cookshoong.www.cookshoongbackend.point.repository.PointReasonReposi
 @RequiredArgsConstructor
 public class PointEventService {
     private static final int SIGNUP_POINT = 4_000;
+    private static final int REVIEW_DEFAULT_POINT = 200;
+    private static final int REVIEW_IMAGE_POINT = 500;
+    private static final String REVIEW_DEFAULT_POINT_EXPLAIN = "리뷰 작성으로 인한 포인트 적립";
+    private static final String REVIEW_IMAGE_POINT_EXPLAIN = "리뷰 작성(사진 첨부)로 인한 포인트 적립";
     private static final String ORDER_COMPLETE_POINT_EXPLAIN = "주문 완료로 인한 포인트 적립";
     private static final String ORDER_ABORT_POINT_EXPLAIN = "주문 취소로 인한 사용 포인트 환불";
     private static final String ORDER_POINT_LOG_EMPTY = "주문코드: `{}`에 대한 포인트 적립이 검색되지 않음.";
@@ -50,6 +56,7 @@ public class PointEventService {
     private final OrderRepository orderRepository;
     private final OrderDetailRepository orderDetailRepository;
     private final ChargeRepository chargeRepository;
+    private final ReviewRepository reviewRepository;
 
     /**
      * 회원가입 시 포인트 제공.
@@ -120,7 +127,7 @@ public class PointEventService {
 
         List<PointLog> pointLogs = pointLogRepository.lookupPointCompleteAmount(order)
             .stream()
-            .filter(pointLog -> pointLog.getPointMovement() > 0)
+            .filter(pointLog -> pointLog.getPointMovement() < 0)
             .collect(Collectors.toList());
 
         if (pointLogs.isEmpty()) {
@@ -134,9 +141,36 @@ public class PointEventService {
 
         PointLog pointLog = pointLogs.get(0);
         Account account = pointLog.getAccount();
-        int pointMovement = pointLog.getPointMovement();
+        int pointMovement = Math.abs(pointLog.getPointMovement());
 
         pointLogRepository.save(
             new PointLog(account, new PointReasonOrder(order, ORDER_ABORT_POINT_EXPLAIN), pointMovement));
+    }
+
+    /**
+     * 리뷰 포인트 생성.
+     *
+     * @param reviewId the review id
+     */
+    public void createReviewPoint(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(ReviewNotFoundException::new);
+
+        pointLogRepository.save(createReviewPointLog(review));
+    }
+
+    private PointLog createReviewPointLog(Review review) {
+        if (review.getReviewHasImages().isEmpty()) {
+            return createReviewPointLog(review, REVIEW_DEFAULT_POINT_EXPLAIN, REVIEW_DEFAULT_POINT);
+        }
+
+        return createReviewPointLog(review, REVIEW_IMAGE_POINT_EXPLAIN, REVIEW_IMAGE_POINT);
+    }
+
+    private PointLog createReviewPointLog(Review review, String explain, int point) {
+        Account account = review.getAccount();
+        PointReasonReview pointReasonReview = new PointReasonReview(review, explain);
+
+        return new PointLog(account, pointReasonReview, point);
     }
 }
